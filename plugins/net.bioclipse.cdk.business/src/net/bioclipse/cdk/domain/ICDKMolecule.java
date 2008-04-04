f48faf53ae05c97b39e8b2c91d06464613ea9156
jonalv <jonalv@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-04-04 10:02:05 +0000

    added getAtomcontainer to ICDKMolecule and a small TODO comment (question style)
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5349 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk.business/src/net/bioclipse/cdk/domain/ICDKMolecule.java
+++ plugins/net.bioclipse.cdk.business/src/net/bioclipse/cdk/domain/ICDKMolecule.java
@@ -3,9 +3,13 @@ package net.bioclipse.cdk.domain;
 import net.bioclipse.core.business.BioclipseException;
 import net.bioclipse.core.domain.IMolecule;
 
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+//TODO: Should this interface extend cdk.interfaces.IMoleule?
 public interface ICDKMolecule extends IMolecule{
 
 	public String getCML() throws BioclipseException;
 	public String getFingerprint() throws BioclipseException;
+	public IAtomContainer getAtomContainer();
 
 }
