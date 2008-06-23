56a97259f64af4ca59e9be994bfda87e144e870e
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-06-23 14:00:10 +0000

    Refactored atomselections by index to core, as not CDK-dependent.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5897 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk.business/src/net/bioclipse/cdk/domain/AtomContainerSelection.java
+++ plugins/net.bioclipse.cdk.business/src/net/bioclipse/cdk/domain/AtomContainerSelection.java
@@ -7,7 +7,7 @@ import org.openscience.cdk.interfaces.IAtomContainer;
  * @author ola
  *
  */
-public class MoleculeSelection {
+public class AtomContainerSelection {
 
     IAtomContainer selection;
 
@@ -27,7 +27,7 @@ public class MoleculeSelection {
      * Constructor
      * @param selection
      */
-    public MoleculeSelection(IAtomContainer selection) {
+    public AtomContainerSelection(IAtomContainer selection) {
 
         super();
         this.selection = selection;
