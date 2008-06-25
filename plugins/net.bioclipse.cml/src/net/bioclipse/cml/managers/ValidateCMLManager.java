9a52cc932b7147a888a1188ea8818f6cf2f291bf
jonalv <jonalv@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-06-25 13:42:13 +0000

    removed @Override from method overriding from an interface. Java 1.5 doesn't support this and Bioclipse is still java 1.5
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@5932 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cml/src/net/bioclipse/cml/managers/ValidateCMLManager.java
+++ plugins/net.bioclipse.cml/src/net/bioclipse/cml/managers/ValidateCMLManager.java
@@ -2,7 +2,6 @@ package net.bioclipse.cml.managers;
 
 public class ValidateCMLManager implements IValidateCMLManager {
 
-	@Override
 	public void validate(String filename) {
 		// TODO Auto-generated method stub
 		//validation goes in here
