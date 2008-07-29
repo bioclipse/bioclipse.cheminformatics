c4e11d54bd999dce8d18825af5c6b460f44bab28
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-07-29 08:14:25 +0000

    Some test editing for the Jumbo bug.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@6294 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk.business.test/src/net/bioclipse/cdk/domain/tests/TestCDKMolecule.java
+++ plugins/net.bioclipse.cdk.business.test/src/net/bioclipse/cdk/domain/tests/TestCDKMolecule.java
@@ -13,6 +13,7 @@ package net.bioclipse.cdk.domain.tests;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.BitSet;
@@ -24,6 +25,7 @@ import net.bioclipse.cdk.business.ICDKManager;
 import net.bioclipse.cdk.domain.CDKMolecule;
 import net.bioclipse.cdk.domain.ICDKMolecule;
 import net.bioclipse.cdk10.business.CDK10Manager;
+import net.bioclipse.cdk10.business.CDK10ManagerHelper;
 import net.bioclipse.cdk10.business.CDK10Molecule;
 import net.bioclipse.core.business.BioclipseException;
 
@@ -31,6 +33,8 @@ import org.eclipse.core.runtime.CoreException;
 import org.junit.Before;
 import org.junit.Test;
 import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.io.IChemObjectReader;
+import org.openscience.cdk.io.ReaderFactory;
 import org.openscience.cdk.isomorphism.UniversalIsomorphismTester;
 
 public class TestCDKMolecule {
@@ -146,6 +150,8 @@ public class TestCDKMolecule {
         
         assertTrue( cdk.fingerPrintMatches( cdkmol, convertedmol ));
 
+        //It seems we cannot test isomorphism since AC have different impl
+        //FIXME: verify by egonw
         assertTrue( UniversalIsomorphismTester
           .isIsomorph( cdk10mol.getAtomContainer(), 
                        cdkmol.getAtomContainer()));        
@@ -162,6 +168,65 @@ public class TestCDKMolecule {
         System.out.println("MassNumber cdk10: " + cdk10mol.getAtomContainer().getAtom( 0 ).getMassNumber());
         System.out.println("MassNumber cdk: " + cdkmol.getAtomContainer().getAtom( 0 ).getMassNumber());
         System.out.println("MassNumber converted: " + convertedmol.getAtomContainer().getAtom( 0 ).getMassNumber());
+        
+    }
+
+    @Test
+    public void testReadCML() throws IOException{
+            
+        String cmlstring="<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>"
++"<molecule id=\"m1\" xmlns=\"http://www.xml-cml.org/schema\">"
++"  <atomArray>"
++"  <atom id=\"a1\" elementType=\"C\" x2=\"0.1542\" y2=\"0.2\" formalCharge=\"0\" hydrogenCount=\"0\" isotopeNumber=\"12\"/>"
++"   <atom id=\"a2\" elementType=\"C\" x2=\"-0.1458\" y2=\"1.05\" formalCharge=\"0\" hydrogenCount=\"0\" isotopeNumber=\"12\"/>"
++"  <atom id=\"a3\" elementType=\"C\" x2=\"1.0292\" y2=\"-0.0875\" formalCharge=\"0\" hydrogenCount=\"0\" isotopeNumber=\"12\"/>"
++"  <atom id=\"a4\" elementType=\"N\" x2=\"0.4542\" y2=\"1.7208\" formalCharge=\"0\" hydrogenCount=\"0\" isotopeNumber=\"14\"/>"
++"  <atom id=\"a5\" elementType=\"S\" x2=\"-0.5708\" y2=\"-0.3292\" formalCharge=\"0\" hydrogenCount=\"0\" isotopeNumber=\"32\"/>"
++"  <atom id=\"a6\" elementType=\"C\" x2=\"1.3375\" y2=\"1.5333\" formalCharge=\"0\" hydrogenCount=\"0\" isotopeNumber=\"12\"/>"
++"  <atom id=\"a7\" elementType=\"O\" x2=\"1.6792\" y2=\"0.5208\" formalCharge=\"0\" hydrogenCount=\"0\" isotopeNumber=\"16\"/>"
++"  <atom id=\"a8\" elementType=\"O\" x2=\"1.6\" y2=\"0.8833\" formalCharge=\"0\" hydrogenCount=\"0\" isotopeNumber=\"16\"/>"
++"  <atom id=\"a9\" elementType=\"C\" x2=\"-1.0333\" y2=\"1.05\" formalCharge=\"0\" hydrogenCount=\"0\" isotopeNumber=\"12\"/>"
++"  <atom id=\"a10\" elementType=\"C\" x2=\"-1.2958\" y2=\"0.1958\" formalCharge=\"0\" hydrogenCount=\"0\" isotopeNumber=\"12\"/>"
++"  <atom id=\"a11\" elementType=\"O\" x2=\"1.2\" y2=\"-0.9667\" formalCharge=\"0\" hydrogenCount=\"0\" isotopeNumber=\"16\"/>"
++"  <atom id=\"a12\" elementType=\"Cl\" x2=\"1.65\" y2=\"3.0458\" formalCharge=\"0\" hydrogenCount=\"0\" isotopeNumber=\"35\"/>"
++"  <atom id=\"a13\" elementType=\"C\" x2=\"1.9292\" y2=\"2.2125\" formalCharge=\"0\" hydrogenCount=\"0\" isotopeNumber=\"12\"/>"
++"  <atom id=\"a14\" elementType=\"C\" x2=\"2.05\" y2=\"-1.25\" formalCharge=\"0\" hydrogenCount=\"0\" isotopeNumber=\"12\"/>"
++"</atomArray>"
++"<bondArray>"
++"  <bond id=\"b1\" atomRefs2=\"a2 a1\" order=\"D\"/>"
++"  <bond id=\"b2\" atomRefs2=\"a3 a1\" order=\"S\"/>"
++"  <bond id=\"b3\" atomRefs2=\"a4 a2\" order=\"S\"/>"
++"  <bond id=\"b4\" atomRefs2=\"a5 a1\" order=\"S\"/>"
++"    <bond id=\"b5\" atomRefs2=\"a6 a4\" order=\"S\"/>"
++"  <bond id=\"b6\" atomRefs2=\"a7 a3\" order=\"D\"/>"
++"  <bond id=\"b7\" atomRefs2=\"a8 a6\" order=\"D\"/>"
++"  <bond id=\"b8\" atomRefs2=\"a9 a2\" order=\"S\"/>"
++"  <bond id=\"b9\" atomRefs2=\"a10 a5\" order=\"S\"/>"
++"  <bond id=\"b10\" atomRefs2=\"a11 a3\" order=\"S\"/>"
++"  <bond id=\"b11\" atomRefs2=\"a12 a13\" order=\"S\"/>"
++"  <bond id=\"b12\" atomRefs2=\"a13 a6\" order=\"S\"/>"
++"  <bond id=\"b13\" atomRefs2=\"a14 a11\" order=\"S\"/>"
++"  <bond id=\"b14\" atomRefs2=\"a9 a10\" order=\"S\"/>"
++"</bondArray>"
++"<scalar dictRef=\"cdk:molecularProperty\" title=\"ROTB\" dataType=\"xsd:string\">5</scalar>"
++"<scalar dictRef=\"cdk:molecularProperty\" title=\"LOGP\" dataType=\"xsd:string\">9.080000000000000e-001</scalar>"
++"<scalar dictRef=\"cdk:molecularProperty\" title=\"INFO\" dataType=\"xsd:string\">TANGIBLE</scalar>"
++"<scalar dictRef=\"cdk:molecularProperty\" title=\"DONH\" dataType=\"xsd:string\">1</scalar>"
++"<scalar dictRef=\"cdk:molecularProperty\" title=\"TPS\" dataType=\"xsd:string\">5.729560000000000e+001</scalar>"
++"<scalar dictRef=\"cdk:molecularProperty\" title=\"IDNUMBER\" dataType=\"xsd:string\">F0326-0475</scalar>"
++"<scalar dictRef=\"cdk:molecularProperty\" title=\"ACCH\" dataType=\"xsd:string\">3</scalar>"
++"<scalar dictRef=\"cdk:molecularProperty\" title=\"SALTID\" dataType=\"xsd:string\">0</scalar>"
++"</molecule>";
+    	
+        ByteArrayInputStream bais=new ByteArrayInputStream(cmlstring.getBytes());
+
+        ReaderFactory readerFactory=new ReaderFactory();
+        CDK10ManagerHelper.registerFormats(readerFactory);
+
+        //Create the reader
+        IChemObjectReader reader= readerFactory.createReader(bais);
+        reader.getFormat();
+        
         
     }
 
