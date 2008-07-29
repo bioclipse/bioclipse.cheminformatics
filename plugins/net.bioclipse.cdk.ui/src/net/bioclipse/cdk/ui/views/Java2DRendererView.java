2ca2dc7253e841aacee93b357c5bccbdbb1bffd7
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-07-29 08:32:16 +0000

    Replaced the getAdapter with the one from cdk10. Still not working, but a start. Needs some work by egonw, see bug 2027732.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@6296 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk.ui/src/net/bioclipse/cdk/ui/views/Java2DRendererView.java
+++ plugins/net.bioclipse.cdk.ui/src/net/bioclipse/cdk/ui/views/Java2DRendererView.java
@@ -17,10 +17,13 @@ package net.bioclipse.cdk.ui.views;
 
 import net.bioclipse.cdk.business.CDKManager;
 import net.bioclipse.cdk.business.ICDKManager;
+import net.bioclipse.cdk.domain.CDKMolecule;
 import net.bioclipse.cdk.domain.ICDKMolecule;
 import net.bioclipse.cdk.ui.Activator;
 import net.bioclipse.cdk.ui.widgets.JChemPaintWidget;
 import net.bioclipse.core.business.BioclipseException;
+import net.bioclipse.core.domain.AtomIndexSelection;
+import net.bioclipse.core.domain.IChemicalSelection;
 import net.bioclipse.core.util.LogUtils;
 
 import org.apache.log4j.Logger;
@@ -35,6 +38,7 @@ import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
+import org.openscience.cdk.AtomContainer;
 import org.openscience.cdk.DefaultChemObjectBuilder;
 import org.openscience.cdk.Molecule;
 import org.openscience.cdk.exception.InvalidSmilesException;
@@ -101,74 +105,102 @@ public class Java2DRendererView extends ViewPart
     private void reactOnSelection(ISelection selection) {
         
         if (!(selection instanceof IStructuredSelection))
-        return;
-        IStructuredSelection ssel = (IStructuredSelection) selection;
-        
-        Object obj = ssel.getFirstElement();
-        
-        //If we have an ICDKMolecule, just get the AC
-        if (obj instanceof ICDKMolecule) {
-            ICDKMolecule mol = (ICDKMolecule) obj;
-            if (mol.getAtomContainer()==null){
-                logger.debug("CDKMolecule but can't get AtomContainer.");
-                return;
-            }
-            setAtomContainer(mol.getAtomContainer());
-        }
-        
-        //Try to get an IMolecule via the adapter
-        else if (obj instanceof IAdaptable) {
-            IAdaptable ada=(IAdaptable)obj;
+            return;
+            IStructuredSelection ssel = (IStructuredSelection) selection;
             
-            Object molobj=ada
-                    .getAdapter( net.bioclipse.core.domain.IMolecule.class );
-            if (molobj==null || 
-                   (!(molobj instanceof net.bioclipse.core.domain.IMolecule ))){
-                //Nothing to show
-                clearView();
-                return;
-            }
-
-            net.bioclipse.core.domain.IMolecule bcmol 
-                                = (net.bioclipse.core.domain.IMolecule) molobj;
+            Object obj = ssel.getFirstElement();
             
-            try {
-
-                //Create cdkmol from IMol, via CML or SMILES if that fails
-                ICDKMolecule cdkMol=cdk.create( bcmol );
-
-                //Create molecule
-                IAtomContainer ac=cdkMol.getAtomContainer();
-                molecule=new Molecule(ac);
-
-                //Create 2D-coordinates if not available
-                if (GeometryTools.has2DCoordinatesNew( molecule )==0){
-                    sdg.setMolecule((IMolecule)molecule.clone());
-                    sdg.generateCoordinates();
-                    molecule = sdg.getMolecule();
+            //If we have an ICDKMolecule, just get the AC
+            if (obj instanceof CDKMolecule) {
+                CDKMolecule mol = (CDKMolecule) obj;
+                if (mol.getAtomContainer()==null){
+                    logger.debug("CDKMolecule but can't get AtomContainer.");
+                    return;
                 }
-                
-                //Set AtomColorer based on active editor
-                //RFE: AtomColorer på JCPWidget
-                //TODO
+                setAtomContainer(mol.getAtomContainer());
+            }
+            
+            //Try to get an IMolecule via the adapter
+            else if (obj instanceof IAdaptable) {
+                IAdaptable ada=(IAdaptable)obj;
 
-                //Update widget
-                setAtomContainer(molecule);
-            } catch (CloneNotSupportedException e) {
-                clearView();
-                logger.debug( "Unable to clone structure in 2Dview: " 
-                              + e.getMessage() );
-            } catch ( BioclipseException e ) {
-                clearView();
-                logger.debug( "Unable to generate structure in 2Dview: " 
-                              + e.getMessage() );
-            } catch ( Exception e ) {
-                clearView();
-                logger.debug( "Unable to generate structure in 2Dview: " 
-                              + e.getMessage() );
+                
+                //Start by requesting molecule
+                Object molobj=ada
+                        .getAdapter( net.bioclipse.core.domain.IMolecule.class );
+                if (molobj==null ){
+                    //Nothing to show
+//                    clearView();
+                    return;
+                }
+
+                net.bioclipse.core.domain.IMolecule bcmol 
+                                    = (net.bioclipse.core.domain.IMolecule) molobj;
+                
+                try {
+
+                    //Create cdkmol from IMol, via CML or SMILES if that fails
+                    ICDKMolecule cdkMol=cdk.create( bcmol );
+
+                    //Create molecule
+                    IAtomContainer ac=cdkMol.getAtomContainer();
+                    molecule=new Molecule(ac);
+
+                    //Create 2D-coordinates if not available
+                    if (GeometryTools.has2DCoordinatesNew( molecule )==0){
+                        sdg.setMolecule((IMolecule)molecule.clone());
+                        sdg.generateCoordinates();
+                        molecule = sdg.getMolecule();
+                    }
+                    
+                    //Set AtomColorer based on active editor
+                    //RFE: AtomColorer på JCPWidget
+                    //TODO
+
+                    //Update widget
+                    setAtomContainer(molecule);
+                } catch (CloneNotSupportedException e) {
+                    clearView();
+                    logger.debug( "Unable to clone structure in 2Dview: " 
+                                  + e.getMessage() );
+                } catch ( BioclipseException e ) {
+                    clearView();
+                    logger.debug( "Unable to generate structure in 2Dview: " 
+                                  + e.getMessage() );
+                } catch ( Exception e ) {
+                    clearView();
+                    logger.debug( "Unable to generate structure in 2Dview: " 
+                                  + e.getMessage() );
+                }
+
+                
+                //Handle case where Iadaptable can return atoms to be highlighted
+                Object selobj=ada
+                .getAdapter( IChemicalSelection.class );
+//                ArrayList<Integer> atomSelectionIndices=new ArrayList<Integer>();
+
+                if (selobj!=null){
+                    IChemicalSelection atomSelection=(IChemicalSelection)selobj;
+                    
+                    if ( atomSelection instanceof AtomIndexSelection ) {
+                        AtomIndexSelection isel = (AtomIndexSelection) atomSelection;
+                        int[] selindices = isel.getSelection();
+//                        System.out.println("\n** Should highlight these JCP atoms:\n");
+                        IAtomContainer selectedMols=new AtomContainer();
+                        for (int i=0; i<selindices.length;i++){
+                            selectedMols.addAtom( molecule.getAtom( selindices[i] ));
+//                            System.out.println(i);
+                        }
+                        canvasView.getRendererModel().setExternalSelectedPart( selectedMols );
+                        canvasView.redraw();
+                    }
+                    
+                    
+                }
+
+                
             }
 
-        }
         
     }
 
