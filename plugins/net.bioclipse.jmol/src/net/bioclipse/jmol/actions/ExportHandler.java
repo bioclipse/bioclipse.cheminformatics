1b5585dd82451d6588b04eea31dc9a723f8ab357
gilleain <gilleain@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-08-13 09:29:46 +0000

    Export an image from JMol as a .png file
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@6575 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.jmol/src/net/bioclipse/jmol/actions/ExportHandler.java
+++ plugins/net.bioclipse.jmol/src/net/bioclipse/jmol/actions/ExportHandler.java
@@ -1,14 +1,24 @@
 package net.bioclipse.jmol.actions;
 
-import java.util.Map;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import net.bioclipse.jmol.editors.JmolEditor;
+import net.bioclipse.jmol.views.JmolPanel;
 
 import org.apache.log4j.Logger;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 
@@ -18,17 +28,33 @@ public class ExportHandler extends AbstractHandler implements IHandler {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		
-		IEditorPart editor=HandlerUtil.getActiveEditor(event);
+		IEditorPart editor = HandlerUtil.getActiveEditor(event);
 		if (!(editor instanceof JmolEditor)) {
-			logger.error("A jmol command was run but jmol is not active editor");
+			logger.error("A jmol command was run but jmol is not the active editor");
 			return null;
 		}
 		JmolEditor jmolEditor = (JmolEditor) editor;
-
-		//FIXME: implement
+		JmolPanel panel = jmolEditor.getJmolPanel();
+		Image image = new BufferedImage(
+				panel.getWidth(),
+				panel.getHeight(),
+				BufferedImage.TYPE_INT_RGB);
+		Graphics g = image.getGraphics();
+		panel.paint(g);
+		
+		FileDialog dialog = new FileDialog(HandlerUtil.getActiveShell(event), SWT.SAVE);
+		dialog.setFilterNames (new String [] {"PNG Files", "All Files (*.*)"});
+		dialog.setFilterExtensions (new String [] {"*.png", "*.*"});
+		String result = dialog.open();
+		
+		if (result != null) {
+			try {
+				ImageIO.write((RenderedImage) image, "PNG", new File(result));
+			} catch (IOException ioe) {
+				logger.error("Problem with the path " + result);
+			}
+		}
 
 		return null;
 	}
-
-	
 }
