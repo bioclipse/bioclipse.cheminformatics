cbb1bed3ffc3105c748a2cb9f3ace5fb33e6a835
goglepox <goglepox@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-09-03 12:24:46 +0000

    Moved to net.bioclipse.cdk.ui.views
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@6859 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.cdk.ui/src/net/bioclipse/cdk/ui/views/IMoleculesEditorModel.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2008 The Bioclipse Project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * <http://www.eclipse.org/legal/epl-v10.html>.
+ * 
+ * Contributors:
+ *     Arvid Berg
+ *     
+ ******************************************************************************/
+package net.bioclipse.cdk.ui.views;
+
+
+/**
+ * @author arvid
+ *
+ */
+public interface IMoleculesEditorModel {
+    
+    public Object getMoleculeAt(int index);
+    
+    public int getNumberOfMolecules();
+    
+    public void save();
+}
