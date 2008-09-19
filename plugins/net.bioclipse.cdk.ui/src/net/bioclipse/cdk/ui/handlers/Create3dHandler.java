f2de10dd6b02bf7731b35edb7f6b17824b6a4f14
shk3 <shk3@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-09-19 13:35:15 +0000

    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@7056 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.cdk.ui/src/net/bioclipse/cdk/ui/handlers/Create3dHandler.java
@@ -0,0 +1,91 @@
+/*******************************************************************************
+ * Copyright (c) 2008 The Bioclipse Project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Stefan Kuhn
+ *     
+ ******************************************************************************/
+package net.bioclipse.cdk.ui.handlers;
+
+
+import net.bioclipse.cdk.business.Activator;
+import net.bioclipse.cdk.domain.ICDKMolecule;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.actions.ActionDelegate;
+import org.eclipse.ui.dialogs.SaveAsDialog;
+
+/**
+ * A handler class for a Generate 2D Coordinates menu item
+ */
+public class Create3dHandler extends ActionDelegate {
+
+	public int answer;
+	/* (non-Javadoc)
+	 * @see org.eclipse.core.commands.AbstractHandler#execute(org.eclipse.core.commands.ExecutionEvent)
+	 */
+	public void run(IAction action){
+		  ISelection sel=PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection();
+		  if (sel.isEmpty()==false){
+		      if (sel instanceof IStructuredSelection) {
+		         IStructuredSelection ssel = (IStructuredSelection) sel;
+		         ICDKMolecule mol;
+				 try {
+					mol = Activator.getDefault().getCDKManager().loadMolecule((IFile)ssel.getFirstElement());
+					mol = (ICDKMolecule)Activator.getDefault().getCDKManager().generate3dCoordinates(mol);
+				 } catch (Exception e) {
+					throw new RuntimeException(e.getMessage());
+				 }
+		         MessageBox mb = new MessageBox(new Shell(), SWT.YES | SWT.NO | SWT.ICON_QUESTION);
+		         mb.setText("Change file");
+		         mb.setMessage("Do you want to write the coordinates into the existing file? If no, a new one will be created.");
+		         int val=mb.open();
+		         if(val==SWT.YES){
+		        	 try {
+						Activator.getDefault().getCDKManager().saveMolecule(mol, (IFile)ssel.getFirstElement(), ((IFile)ssel.getFirstElement()).getFileExtension());
+					} catch (Exception e) {
+						throw new RuntimeException(e.getMessage());
+					}
+		         }else{
+		        	 SaveAsDialog dialog=new SaveAsDialog(new Shell());
+		        	 int saveasreturn = dialog.open();
+		        	 IPath result=dialog.getResult();
+		        	 if(saveasreturn!=SaveAsDialog.CANCEL){
+			        	 if(dialog.getResult().getFileExtension()==null)
+			        		 result=result.addFileExtension(((IFile)ssel.getFirstElement()).getFileExtension());
+			             try {
+			     	    	if(((IFile)ssel.getFirstElement()).getWorkspace().getRoot().getFile(result).exists()){
+				   		        new Shell().getDisplay().syncExec(new Runnable() {
+				   		        	public void run() { 
+						   		         MessageBox mb = new MessageBox(new Shell(), SWT.YES | SWT.NO | SWT.ICON_QUESTION);
+						   		         mb.setText("File exists");
+						   		         mb.setMessage("This file already exists. Do you want to overwrite it?");
+						   		         Create3dHandler.this.answer=mb.open();
+				   		        	}});
+				   		        if(answer==SWT.YES)
+				   		        	Activator.getDefault().getCDKManager().saveMolecule(mol, ((IFile)ssel.getFirstElement()).getWorkspace().getRoot().getFile(result), ((IFile)ssel.getFirstElement()).getFileExtension());
+			     	    	} else {
+			     	    		Activator.getDefault().getCDKManager().saveMolecule(mol, ((IFile)ssel.getFirstElement()).getWorkspace().getRoot().getFile(result), ((IFile)ssel.getFirstElement()).getFileExtension());
+			     	    	}
+						 } catch (Exception e) {
+							throw new RuntimeException(e.getMessage());
+						 }
+		        	 }
+		         }
+		      }
+		  }		
+	}
+
+}
