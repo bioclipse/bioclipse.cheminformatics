e386ce7c62d19ef4247b51c74d560a437860ac99
goglepox <goglepox@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-11-26 08:45:17 +0000

    Added menu contribution to JChemPaint and implemented decharge handler to use IChemModelRelay
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@7838 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk.jchempaint/src/net/bioclipse/cdk/jchempaint/handlers/AddHandler.java
+++ plugins/net.bioclipse.cdk.jchempaint/src/net/bioclipse/cdk/jchempaint/handlers/AddHandler.java
@@ -4,38 +4,57 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Arvid Berg
- *     
+ *
  ******************************************************************************/
 package net.bioclipse.cdk.jchempaint.handlers;
+
+//import net.bioclipse.cdk.jchempaint.business.Activator;
+//import net.bioclipse.cdk.jchempaint.business.IJChemPaintManager;
 
 import org.apache.log4j.Logger;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.expressions.IEvaluationContext;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.openscience.cdk.controller.Controller2DHub;
 import org.openscience.cdk.controller.IController2DModel;
 import org.openscience.cdk.controller.IController2DModel.DrawMode;
+import org.openscience.cdk.interfaces.IAtom;
 
 public class AddHandler extends AbstractHandler implements IHandler {
 
-    Logger logger = Logger.getLogger(RemoveHandler.class);
+    Logger logger = Logger.getLogger(AddHandler.class);
     public Object execute(ExecutionEvent event) throws ExecutionException {
-       IEditorPart editor=HandlerUtil.getActiveEditor(event);
-       if(! (editor instanceof net.bioclipse.cdk.jchempaint.editor.JChemPaintEditor)){
-           logger.debug("Not JChemPaintEditor");
-           return null;
+//       IJChemPaintManager jcp= Activator.getDefault().getExampleManager();
+       Object object = event.getApplicationContext();
+
+       ISelection selection = HandlerUtil.getCurrentSelection( event );
+       if(selection instanceof IStructuredSelection ) {
+           Object element = ((IStructuredSelection)selection).getFirstElement();
+           if(element instanceof IAtom) {
+//               jcp.addAtom( "C", (IAtom) element);
+           }
+
        }
-       
-       Controller2DHub hub=((net.bioclipse.cdk.jchempaint.editor.JChemPaintEditor)editor).getControllerHub();
-       IController2DModel c2dm=hub.getController2DModel();       
-       c2dm.setDrawMode(DrawMode.DRAWBOND);
-        return null;
+       return null;
     }
 
+    protected IAtom getSingleSelectedAtom(ISelection selection) {
+
+        if(selection instanceof IStructuredSelection ) {
+            Object element = ((IStructuredSelection)selection).getFirstElement();
+            if(element instanceof IAtom) {
+                return (IAtom)element;
+            }
+        }
+        return null;
+    }
 }
--- plugins/net.bioclipse.cdk.jchempaint/src/net/bioclipse/cdk/jchempaint/handlers/DecchargeHandler.java
+++ plugins/net.bioclipse.cdk.jchempaint/src/net/bioclipse/cdk/jchempaint/handlers/DecchargeHandler.java
@@ -4,10 +4,10 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Arvid Berg
- *     
+ *
  ******************************************************************************/
 package net.bioclipse.cdk.jchempaint.handlers;
 
@@ -18,26 +18,43 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.openscience.cdk.controller.Controller2DHub;
+import org.openscience.cdk.controller.IChemModelRelay;
 import org.openscience.cdk.controller.IController2DModel;
 import org.openscience.cdk.controller.IController2DModel.DrawMode;
+import org.openscience.cdk.interfaces.IAtom;
 
 public class DecchargeHandler extends AbstractHandler implements IHandler {
 
     Logger logger = Logger.getLogger(RemoveHandler.class);
     public Object execute(ExecutionEvent event) throws ExecutionException {
+
+
        IEditorPart editor=HandlerUtil.getActiveEditor(event);
-       if(! (editor instanceof net.bioclipse.cdk.jchempaint.editor.JChemPaintEditor)){
-           logger.debug("Not JChemPaintEditor");
-           return null;
+
+       if( (editor instanceof JChemPaintEditor)) {
+           IChemModelRelay relay =  ((JChemPaintEditor)editor).getControllerHub();
+
+           IAtom selected = getSingleSelectedAtom( event );
+           if(selected != null) {
+               relay.setCharge( selected, (int)(selected.getCharge()-1 ));
+           }
        }
-       
-       Controller2DHub hub=((JChemPaintEditor)editor).getControllerHub();
-       IController2DModel c2dm=hub.getController2DModel();       
-       c2dm.setDrawMode(DrawMode.DECCHARGE);
-        return null;
+       return null;
     }
 
+    protected IAtom getSingleSelectedAtom(ExecutionEvent event) {
+        ISelection selection = HandlerUtil.getCurrentSelection( event );
+        if(selection instanceof IStructuredSelection ) {
+            Object element = ((IStructuredSelection)selection).getFirstElement();
+            if(element instanceof IAtom) {
+                return (IAtom)element;
+            }
+        }
+        return null;
+    }
 }
