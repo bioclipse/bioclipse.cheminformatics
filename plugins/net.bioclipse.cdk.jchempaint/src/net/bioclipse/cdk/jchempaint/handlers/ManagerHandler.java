66f94e74a928490db1399296020968561c5c32f8
goglepox <goglepox@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-11-27 17:21:16 +0000

    New commands handler that calls a manager method with reflection
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@7890 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.cdk.jchempaint/src/net/bioclipse/cdk/jchempaint/handlers/ManagerHandler.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (c) 2008 The Bioclipse Project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * <http://www.eclipse.org/legal/epl-v10.html>.
+ * 
+ * Contributors:
+ *     Arvid Berg goglepox@users.sf.se
+ *     
+ ******************************************************************************/
+package net.bioclipse.cdk.jchempaint.handlers;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import net.bioclipse.cdk.jchempaint.business.IJChemPaintManager;
+import net.bioclipse.core.util.LogUtils;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+
+/**
+ * @author arvid
+ *
+ */
+public class ManagerHandler extends AbstractJChemPaintHandler {
+
+    public static final String managerMethod = "jcp.manager.method";
+    
+    Logger logger = Logger.getLogger( ManagerHandler.class );
+    /* (non-Javadoc)
+     * @see org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.ExecutionEvent)
+     */
+    public Object execute( ExecutionEvent event ) throws ExecutionException {
+        
+        String methodName = event.getParameter( managerMethod  );
+        Class<IJChemPaintManager> cl = IJChemPaintManager.class;
+        try {
+            Method method = cl.getDeclaredMethod(  methodName ,new Class[] {} );
+           method.invoke( getManager(), new Object[] {} ); 
+        } catch ( NoSuchMethodException e ) {
+            // TODO Auto-generated catch block
+            LogUtils.debugTrace( logger, e );
+        } catch ( IllegalArgumentException e ) {
+            // TODO Auto-generated catch block
+            LogUtils.debugTrace( logger, e );
+        } catch ( IllegalAccessException e ) {
+            // TODO Auto-generated catch block
+            LogUtils.debugTrace( logger, e );
+        } catch ( InvocationTargetException e ) {
+            // TODO Auto-generated catch block
+            LogUtils.debugTrace( logger, e );
+        }
+        return null;
+    }
+
+}
