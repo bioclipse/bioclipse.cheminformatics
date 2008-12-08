f7c202f12fee3280eb254e000a438d57f1f69684
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-12-08 11:44:09 +0000

    Copied cdk.debug.test as template for inchi.business.test
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@7997 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.inchi.business.test/src/net/bioclipse/cdk/debug/test/CDKDebugManagerTest.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2008 The Bioclipse Project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Ola Spjuth
+ *     Jonathan Alvarsson
+ *
+ ******************************************************************************/
+package net.bioclipse.cdk.debug.test;
+
+import net.bioclipse.cdkdebug.business.CDKDebugManager;
+import net.bioclipse.cdkdebug.business.ICDKDebugManager;
+import net.bioclipse.core.business.IBioclipseManager;
+import net.bioclipse.core.tests.AbstractManagerTest;
+
+public class CDKDebugManagerTest extends AbstractManagerTest {
+
+    ICDKDebugManager cdk;
+
+    //Do not use SPRING OSGI for this manager
+    //since we are only testing the implementations of the manager methods
+    public CDKDebugManagerTest() {
+        cdk = new CDKDebugManager();
+    }
+
+    public IBioclipseManager getManager() {
+        return cdk;
+    }
+
+}
