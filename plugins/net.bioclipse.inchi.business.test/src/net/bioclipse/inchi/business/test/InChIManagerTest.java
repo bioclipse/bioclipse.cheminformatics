9f79b4deec9e5d9dd3d52aa54d627684eb85977c
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-12-08 11:46:18 +0000

    Added the required unit tests
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@7999 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.inchi.business.test/src/net/bioclipse/inchi/business/test/InChIManagerTest.java
+++ plugins/net.bioclipse.inchi.business.test/src/net/bioclipse/inchi/business/test/InChIManagerTest.java
@@ -1,21 +1,24 @@
 /*******************************************************************************
- * Copyright (c) 2008 The Bioclipse Project and others.
+ * Copyright (c) 2008  Egon Willighagen <egonw@users.sf.net>
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *     Ola Spjuth
- *     Jonathan Alvarsson
- *
+ * Contact: http://www.bioclipse.net/
  ******************************************************************************/
 package net.bioclipse.inchi.business.test;
 
+import junit.framework.Assert;
+import net.bioclipse.cdk.business.CDKManager;
 import net.bioclipse.core.business.IBioclipseManager;
+import net.bioclipse.core.domain.IMolecule;
 import net.bioclipse.core.tests.AbstractManagerTest;
 import net.bioclipse.inchi.business.IInChIManager;
 import net.bioclipse.inchi.business.InChIManager;
+
+import org.junit.Test;
 
 public class InChIManagerTest extends AbstractManagerTest {
 
@@ -29,6 +32,24 @@ public class InChIManagerTest extends AbstractManagerTest {
 
     public IBioclipseManager getManager() {
         return inchi;
+    }
+
+    @Test public void testGenerate() throws Exception {
+        CDKManager cdk = new CDKManager();
+        IMolecule mol = cdk.fromSMILES("C");
+        Assert.assertNotNull("Input structure is unexpectedly null", mol);
+        String inchiStr = inchi.generate(mol);
+        Assert.assertNotNull(inchiStr);
+        Assert.assertEquals("InChI=1/CH4/h1H4", inchiStr);
+    }
+
+    @Test public void testGenerateKey() throws Exception {
+        CDKManager cdk = new CDKManager();
+        IMolecule mol = cdk.fromSMILES("C");
+        Assert.assertNotNull("Input structure is unexpectedly null", mol);
+        String key = inchi.generateKey(mol);
+        Assert.assertNotNull(key);
+        Assert.assertEquals("VNWKTOKETHGBQD-UHFFFAOYAM", key);
     }
 
 }
