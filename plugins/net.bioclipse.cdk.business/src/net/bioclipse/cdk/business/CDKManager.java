ca6b4a5bb379022b10e8f431859efea39f467c12
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-12-11 10:07:44 +0000

    Code was OK, but as you said, there actually is no need to cast to a CDK IMolecule first
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@8021 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk.business/src/net/bioclipse/cdk/business/CDKManager.java
+++ plugins/net.bioclipse.cdk.business/src/net/bioclipse/cdk/business/CDKManager.java
@@ -211,20 +211,12 @@ public class CDKManager implements ICDKManager {
 				}
 
 				CDKMolecule mol = new CDKMolecule(ac);
-				String moleculeName = "Molecule " + i;
-
-				// FIXME: Egon, please verify this.
-				// Why must it be an org.openscience.cdk.interfaces.IMolecule in order to have a title?
-				// Can an AC not have a title?
-				if (ac instanceof org.openscience.cdk.interfaces.IMolecule) {
-					org.openscience.cdk.interfaces.IMolecule imol = (org.openscience.cdk.interfaces.IMolecule) ac;
-					String molName = (String) imol
-							.getProperty(CDKConstants.TITLE);
-					if (molName != null && (!(molName.equals("")))) {
-						moleculeName = molName;
-					}
+				String molName = (String)ac.getProperty(CDKConstants.TITLE);
+				if (molName != null && !(molName.length() > 0)) {
+            mol.setName(molName);
+				} else {
+				    mol.setName("Molecule " + i);
 				}
-				mol.setName(moleculeName);
 
 				moleculesList.add(mol);
 			}
