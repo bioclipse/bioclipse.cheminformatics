b21985692ffc7a289cf00e1587593eafd7a02d62
jonalv <jonalv@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-12-17 13:47:34 +0000

    Cleaned up code in MoleculeFileContentProvider. I will close bug #182 now.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@8076 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.chemoinformatics/src/net/bioclipse/chemoinformatics/contentlabelproviders/MoleculeFileContentProvider.java
+++ plugins/net.bioclipse.chemoinformatics/src/net/bioclipse/chemoinformatics/contentlabelproviders/MoleculeFileContentProvider.java
@@ -23,56 +23,68 @@ import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.Viewer;
 
 /** 
- * A class implementing ITreeContentProvider and only returning child elements which are 
- * molecule files. This can be used to build TreeViewers for browsing for molecules.
+ * A class implementing ITreeContentProvider and only returning child elements 
+ * which are molecule files. This can be used to build TreeViewers for browsing 
+ * for molecules.
  *
  */
 public class MoleculeFileContentProvider implements ITreeContentProvider {
 
-	private static final Logger logger = Logger.getLogger(MoleculeFileContentProvider.class);
-	
-	public MoleculeFileContentProvider() {
-	}
-	
-	public void dispose() {
+    private static final Logger logger 
+        = Logger.getLogger(MoleculeFileContentProvider.class);
 
-	}
+    public MoleculeFileContentProvider() {
+    }
 
-	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-	}
+    public void dispose() {
+    }
 
-	public Object[] getElements(Object inputElement) {
-		return getChildren(inputElement);
-	}
+    public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+    }
 
-	public Object[] getChildren(Object parentElement) {
-		ArrayList<IResource> childElements = new ArrayList<IResource>();
-		if(parentElement instanceof IContainer && ((IContainer)parentElement).isAccessible()){
-			try {
-				for(int i=0;i<((IContainer)parentElement).members().length;i++){
-					if(((IContainer)parentElement).members()[i] instanceof IFile 
-					  && (((IFile)((IContainer)parentElement).members()[i]).getFileExtension()!=null 
-					  && ((((IFile)((IContainer)parentElement).members()[i]).getFileExtension().equals(ICDKManager.mol)) 
-					  || ((IFile)((IContainer)parentElement).members()[i]).getFileExtension().equals(ICDKManager.cml)))){
-						    childElements.add(((IContainer)parentElement).members()[i]);
-					}
-					if(((IContainer)parentElement).members()[i] instanceof IContainer && ((IContainer)parentElement).members()[i].isAccessible()){
-						childElements.add(((IContainer)parentElement).members()[i]);
-					}
-				}
-			} catch (CoreException e) {
-				LogUtils.handleException(e,logger);
-			}
-		}
-		return childElements.toArray();
-	}
+    public Object[] getElements(Object inputElement) {
+        return getChildren(inputElement);
+    }
 
-	public Object getParent(Object element) {
-		return ((IFolder)element).getParent();
-	}
+    public Object[] getChildren(Object parentElement) {
 
-	public boolean hasChildren(Object element) {
-		return getChildren(element).length>0;
-	}
+        ArrayList<IResource> childElements = new ArrayList<IResource>();
+        
+        if ( parentElement instanceof IContainer 
+             && ( (IContainer)parentElement ).isAccessible() ) {
+            
+            IContainer container = (IContainer)parentElement;
+            try {
+                for ( int i=0 ; i < container.members().length ; i++ ) {
+                    IResource resource = container.members()[i];
+                    if ( resource instanceof IFile  ) {
+                        String fileExtension 
+                            = ( (IFile)resource ).getFileExtension();
+                        if ( fileExtension != null 
+                             && ( fileExtension.equals(ICDKManager.mol) 
+                               || fileExtension.equals(ICDKManager.cml) ) ) {
+                               
+                               childElements.add(resource);
+                           }
+                    }
+                    if ( resource instanceof IContainer 
+                         && resource.isAccessible() ) {
+                        childElements.add(resource);
+                    }
+                }
+            } 
+            catch (CoreException e) {
+                LogUtils.handleException(e,logger);
+            }
+        }
+        return childElements.toArray();
+    }
 
+    public Object getParent(Object element) {
+        return ( (IFolder)element ).getParent();
+    }
+
+    public boolean hasChildren(Object element) {
+        return getChildren(element).length > 0;
+    }
 }
