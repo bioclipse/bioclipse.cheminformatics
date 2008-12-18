c64815e9c6385e2c60ca2e27e293ff833949a681
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-12-18 09:02:25 +0000

    Tests, pluginTests, and testdata for bug #187: sybyl atom typing on 232 mols SDF.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@8085 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk.debug.test/src/net/bioclipse/cdk/debug/test/CDKDebugManagerPluginTest.java
+++ plugins/net.bioclipse.cdk.debug.test/src/net/bioclipse/cdk/debug/test/CDKDebugManagerPluginTest.java
@@ -11,26 +11,41 @@
  *
  ******************************************************************************/
 package net.bioclipse.cdk.debug.test;
+
+import static org.junit.Assert.fail;
+
+import java.lang.reflect.InvocationTargetException;
+import java.net.URI;
+import java.net.URL;
+import java.util.List;
+
 import net.bioclipse.cdk.business.CDKManager;
 import net.bioclipse.cdk.domain.ICDKMolecule;
 import net.bioclipse.cdkdebug.business.CDKDebugManager;
 import net.bioclipse.cdkdebug.business.ICDKDebugManager;
 import net.bioclipse.core.business.IBioclipseManager;
+import org.eclipse.core.runtime.FileLocator;
 import org.junit.Test;
+
 public class CDKDebugManagerPluginTest {
+
     ICDKDebugManager debug;
+
     public CDKDebugManagerPluginTest() {
         debug = new CDKDebugManager();
     }
+
     public IBioclipseManager getManager() {
         return debug;
     }
+
     @Test public void testDebug() throws Exception {
         CDKManager cdk = new CDKManager();
         ICDKMolecule mol = (ICDKMolecule)cdk.fromSMILES("C");
         debug.debug(mol);
         // would like to test more, but the method does not return anything
     }
+
     @Test public void testDiff() throws Exception {
         CDKManager cdk = new CDKManager();
         ICDKMolecule mol1 = (ICDKMolecule)cdk.fromSMILES("C");
@@ -38,10 +53,44 @@ public class CDKDebugManagerPluginTest {
         debug.diff(mol1, mol2);
         // would like to test more, but the method does not return anything
     }
+
     @Test public void testDepictSybylAtomTypes() throws Exception {
         CDKManager cdk = new CDKManager();
         ICDKMolecule mol = (ICDKMolecule)cdk.fromSMILES("CNCO");
         debug.perceiveSybylAtomTypes(mol);
         // would like to test more, but the method does not return anything
     }
+
+    /**
+     * Test that sybyl atom typing for 232 mols in an SDF does not
+     * throw exception
+     * @throws Exception
+     */
+    @Test public void testDepictSybylAtomTypesFromSDF() throws Exception {
+        CDKManager cdk = new CDKManager();
+        
+        URI uri = getClass().getResource("/testFiles/m2d_ref_232.sdf").toURI();
+        URL url=FileLocator.toFileURL(uri.toURL());
+        String path=url.getFile();
+        List<ICDKMolecule> mols = cdk.loadMolecules( path);
+        
+        int cnt=0;
+        for (ICDKMolecule mol : mols){
+            try {
+				debug.perceiveSybylAtomTypes(mol);
+			} catch (InvocationTargetException e) {
+	        	System.out.println("Atom typing of molecule " + cnt + " failed.");
+				e.printStackTrace();
+				fail("Atom typing of molecule " + cnt + " failed due to: " + e.getMessage() );
+			} catch (NullPointerException e) {
+	        	System.out.println("Atom typing of molecule " + cnt + " failed.");
+				e.printStackTrace();
+				fail("Atom typing of molecule " + cnt + " failed due to NPE: " + e.getMessage() );
+			}
+            // would like to test more, but the method does not return anything
+			cnt++;
+        }
+
+    }
+
 }
--- plugins/net.bioclipse.cdk.debug.test/src/net/bioclipse/cdk/debug/test/CDKDebugManagerTest.java
+++ plugins/net.bioclipse.cdk.debug.test/src/net/bioclipse/cdk/debug/test/CDKDebugManagerTest.java
@@ -11,16 +11,75 @@
  *
  ******************************************************************************/
 package net.bioclipse.cdk.debug.test;
+
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URI;
+import java.net.URL;
+import java.util.List;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.FileLocator;
+import org.junit.Test;
+
+import net.bioclipse.cdk.business.CDKManager;
+import net.bioclipse.cdk.domain.ICDKMolecule;
 import net.bioclipse.cdkdebug.business.CDKDebugManager;
 import net.bioclipse.cdkdebug.business.ICDKDebugManager;
+import net.bioclipse.core.MockIFile;
+import net.bioclipse.core.business.BioclipseException;
 import net.bioclipse.core.business.IBioclipseManager;
 import net.bioclipse.core.tests.AbstractManagerTest;
+
 public class CDKDebugManagerTest extends AbstractManagerTest {
+
     ICDKDebugManager debug;
+
     public CDKDebugManagerTest() {
         debug = new CDKDebugManager();
     }
+
     public IBioclipseManager getManager() {
         return debug;
     }
+    
+
+    /**
+     * Test that sybyl atom typing for 232 mols in an SDF does not
+     * throw exception
+     * @throws CoreException 
+     * @throws BioclipseException 
+     * @throws IOException 
+     * @throws FileNotFoundException 
+     * @throws Exception
+     */
+    @Test public void testDepictSybylAtomTypesFromSDF() throws FileNotFoundException, IOException, BioclipseException, CoreException{
+        CDKManager cdk = new CDKManager();
+
+        String path = getClass().getResource("/testFiles/m2d_ref_232.sdf").getPath();
+        List<ICDKMolecule> mols = cdk.loadMolecules( new MockIFile(path), null);
+
+        int cnt=0;
+        for (ICDKMolecule mol : mols){
+            try {
+				debug.perceiveSybylAtomTypes(mol);
+			} catch (InvocationTargetException e) {
+	        	System.out.println("Atom typing of molecule " + cnt + " failed.");
+				e.printStackTrace();
+				fail("Atom typing of molecule " + cnt + " failed due to: " + e.getMessage() );
+			} catch (NullPointerException e) {
+	        	System.out.println("Atom typing of molecule " + cnt + " failed.");
+				e.printStackTrace();
+				fail("Atom typing of molecule " + cnt + " failed due to NPE: " + e.getMessage() );
+			}
+            // would like to test more, but the method does not return anything
+			cnt++;
+        }
+
+    }
+
+
 }
