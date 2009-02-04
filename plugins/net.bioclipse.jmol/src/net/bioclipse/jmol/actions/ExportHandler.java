598337583a451f6db05a3a015d0edc9c1fdc9794
gilleain <gilleain@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-02-04 16:50:40 +0000

    Fixes bug #335 by using the eclipse SaveAsDialog rather than the platform-native dialog, which was causing problems on osx.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@8593 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.jmol/src/net/bioclipse/jmol/actions/ExportHandler.java
+++ plugins/net.bioclipse.jmol/src/net/bioclipse/jmol/actions/ExportHandler.java
@@ -2,14 +2,13 @@ package net.bioclipse.jmol.actions;
 
 import net.bioclipse.jmol.Activator;
 
-import org.apache.log4j.Logger;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.dialogs.SaveAsDialog;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
@@ -20,16 +19,17 @@ import org.eclipse.ui.handlers.HandlerUtil;
  */
 public class ExportHandler extends AbstractHandler implements IHandler {
 
-    private static final Logger logger = Logger.getLogger(ExportHandler.class);
-
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		Shell shell = HandlerUtil.getActiveShell(event);
-		FileDialog dialog = new FileDialog(shell, SWT.SAVE);
-		dialog.setFilterNames(new String [] {"PNG Files", "All Files (*.*)"});
-		dialog.setFilterExtensions(new String [] {"*.png", "*.*"});
-		String result = dialog.open();
 
-		Activator.getDefault().getJmolManager().snapshot(result);
+		SaveAsDialog dialog = new SaveAsDialog(shell); 
+		int returnValue = dialog.open();
+
+		if (returnValue == SaveAsDialog.OK) {
+		    IPath path = dialog.getResult();
+		    Activator.getDefault().getJmolManager().snapshot(
+		            path.toPortableString());
+		}
 
 		return null;
 	}
