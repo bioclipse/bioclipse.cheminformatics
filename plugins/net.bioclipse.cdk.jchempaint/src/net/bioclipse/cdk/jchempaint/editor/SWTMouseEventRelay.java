2bb1b56d1f8a1514ad27611c5fa5851d53acd5de
goglepox <goglepox@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-02-09 08:38:34 +0000

    SWTMouseRelay now only relays left button clicks (resolves bug #413)
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@8667 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk.jchempaint/src/net/bioclipse/cdk/jchempaint/editor/SWTMouseEventRelay.java
+++ plugins/net.bioclipse.cdk.jchempaint/src/net/bioclipse/cdk/jchempaint/editor/SWTMouseEventRelay.java
@@ -4,10 +4,10 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Arvid Berg
- *     
+ *
  ******************************************************************************/
 package net.bioclipse.cdk.jchempaint.editor;
 
@@ -21,21 +21,21 @@ import org.eclipse.swt.widgets.Listener;
 import org.openscience.cdk.controller.IMouseEventRelay;
 
 public class SWTMouseEventRelay implements MouseListener,MouseMoveListener,Listener{
-    
+
     Logger logger = Logger.getLogger( SWTMouseEventRelay.class );
-    
+
 	private int dragFromX = 0;
 	private int dragFromY = 0;
 	private boolean isDragging = false;
 	private IMouseEventRelay relay;
-	
+
 	public SWTMouseEventRelay(IMouseEventRelay relay) {
 		this.relay=relay;
 	}
-	
+
 	public void handleEvent(Event event) {
         switch (event.type) {
-        
+
         case SWT.MouseEnter:
         	relay.mouseEnter(event.x, event.y);
         	break;
@@ -43,38 +43,31 @@ public class SWTMouseEventRelay implements MouseListener,MouseMoveListener,Liste
           isDragging = false;
         	relay.mouseExit(event.x, event.y);
         	break;
-        	case SWT.MouseDown:
-        	logger.debug( "SWT.MouseDown, should not get here" );
-          break;
-        case SWT.MouseMove:
-            logger.debug( "SWT.MouseMove, should not get here" );
-        	break;
-        case SWT.MouseUp:
-            logger.debug( "SWT.MouseUp, should not get here" );
-          break;
-        case SWT.MouseDoubleClick:
-            logger.debug( "SWT.MouseDoubleClick, should not get here" );
-        	break;
+        	default:
+        	    logger.debug( "Event("+ event.type + ") not supportet" );
         }
       }
 
 	public void mouseDoubleClick(MouseEvent event) {
-		relay.mouseClickedDouble(event.x, event.y);		
+		relay.mouseClickedDouble(event.x, event.y);
 	}
 
 	public void mouseDown(MouseEvent event) {
-		relay.mouseClickedDown(event.x, event.y);
-		if( ((MouseEvent)event).button == 1) {
-        dragFromX=event.x;
-        dragFromY=event.y;
-        isDragging=true;
-    }
+	    if( ((MouseEvent)event).button == 1) {
+	        relay.mouseClickedDown(event.x, event.y);
+	        dragFromX=event.x;
+	        dragFromY=event.y;
+	        isDragging=true;
+	    }
 	}
 
-	public void mouseUp(MouseEvent event) {
-		relay.mouseClickedUp(event.x, event.y);
-    	isDragging=false;
-	}
+	public void mouseUp( MouseEvent event ) {
+
+        if ( ((MouseEvent) event).button == 1 ) {
+            relay.mouseClickedUp( event.x, event.y );
+        }
+        isDragging = false;
+    }
 
 	public void mouseMove(MouseEvent event) {
 		if(isDragging){
