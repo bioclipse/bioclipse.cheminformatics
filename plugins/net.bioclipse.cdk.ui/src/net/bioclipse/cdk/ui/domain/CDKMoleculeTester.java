fdb6a4b9c3c439d9678addae516ebca395e8bd44
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-02-09 14:44:16 +0000

    Implemented a PropertyTester that can be used to target actions for ICDKMolecules having distinct properties. Currently has2D and has3D are implemented but could easily be extended.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@8681 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.cdk.ui/src/net/bioclipse/cdk/ui/domain/CDKMoleculeTester.java
@@ -0,0 +1,50 @@
+package net.bioclipse.cdk.ui.domain;
+
+import net.bioclipse.cdk.business.Activator;
+import net.bioclipse.cdk.business.ICDKManager;
+import net.bioclipse.cdk.domain.ICDKMolecule;
+import net.bioclipse.core.business.BioclipseException;
+
+import org.eclipse.core.expressions.PropertyTester;
+
+public class CDKMoleculeTester extends PropertyTester{
+
+	public boolean test(Object receiver, String property, Object[] args,
+			Object expectedValue) {
+
+		if (!(receiver instanceof ICDKMolecule)) return false;
+
+		ICDKManager cdk=Activator.getDefault().getCDKManager();
+		ICDKMolecule cdkmol = (ICDKMolecule) receiver;
+		
+
+		try {
+			if ("has3d".equalsIgnoreCase(property)){
+				boolean has3d=cdk.has3d(cdkmol);
+				if (expectedValue instanceof Boolean) {
+					boolean expected=(Boolean)expectedValue;
+					if (has3d==expected) return true;
+				}
+				else return false;
+			}
+
+			if ("has2d".equalsIgnoreCase(property)){
+				boolean has2d=cdk.has2d(cdkmol);
+				if (expectedValue instanceof Boolean) {
+					boolean expected=(Boolean)expectedValue;
+					if (has2d==expected) return true;
+				}
+				else return false;
+			}
+
+		} catch (BioclipseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+
+
+
+}
