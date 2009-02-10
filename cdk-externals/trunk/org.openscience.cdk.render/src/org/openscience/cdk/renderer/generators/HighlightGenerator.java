cbca2a0f8525c2526b9eef39759c3fc4d51c4de7
goglepox <goglepox@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-02-10 16:49:18 +0000

    Draws bond highlight as a oval
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@8736 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- cdk-externals/trunk/org.openscience.cdk.render/src/org/openscience/cdk/renderer/generators/HighlightGenerator.java
+++ cdk-externals/trunk/org.openscience.cdk.render/src/org/openscience/cdk/renderer/generators/HighlightGenerator.java
@@ -1,6 +1,9 @@
 package org.openscience.cdk.renderer.generators;
 
+import java.awt.geom.AffineTransform;
+
 import javax.vecmath.Point2d;
+import javax.vecmath.Vector2d;
 
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
@@ -8,6 +11,8 @@ import org.openscience.cdk.interfaces.IBond;
 import org.openscience.cdk.renderer.RendererModel;
 import org.openscience.cdk.renderer.elements.ElementGroup;
 import org.openscience.cdk.renderer.elements.IRenderingElement;
+import org.openscience.cdk.renderer.elements.IRenderingVisitor;
+import org.openscience.cdk.renderer.elements.LineElement;
 import org.openscience.cdk.renderer.elements.OvalElement;
 
 /**
@@ -37,7 +42,42 @@ public class HighlightGenerator extends BasicBondGenerator
     public IRenderingElement generate(IAtomContainer ac, IBond bond) {
         if (bond != null) {
             super.ringSet = super.getRingSet(ac);
-            return super.generate(bond);
+            final ElementGroup group = new ElementGroup();
+            IRenderingElement lines = generate( bond );
+                       IRenderingVisitor v = new IRenderingVisitor() {
+            
+                        public void visit( IRenderingElement element ) {
+            
+                            if(element instanceof ElementGroup) {
+                                ((ElementGroup) element).visitChildren( this );
+                            } else if (element instanceof LineElement) {
+                                LineElement line = (LineElement)element;
+                                Point2d point = new Point2d();
+                                point.interpolate(
+                                           new Point2d(line.x2,line.y2), 
+                                           new Point2d(line.x1,line.y1),
+                                           .5);
+                                group.add( new OvalElement(
+                                            point.x,point.y,
+                                            model.getHighlightRadiusModel(),
+                                            model.getHoverOverColor()
+                                ) );
+//                                group.add( new LineElement( line.x1, line.y1,
+//                                                            line.x2, line.y2,
+//                                                            line.width*3,
+//                                                       model.getHoverOverColor()
+//                                                           ));
+                            }
+                        }
+
+                        public void setTransform( AffineTransform transform ) {
+
+                            // TODO Auto-generated method stub
+                            
+                        }
+                        };
+                        lines.accept( v );
+                        return group;
         } else {
             return new ElementGroup();
         }
