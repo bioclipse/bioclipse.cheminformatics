591993da80ef98d006d9430d8d60c5469e3e1675
goglepox <goglepox@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-02-17 11:53:21 +0000

    Removed tabs and fixed indentation
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@8874 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk.jchempaint/src/net/bioclipse/cdk/jchempaint/editor/JChemPaintEditor.java
+++ plugins/net.bioclipse.cdk.jchempaint/src/net/bioclipse/cdk/jchempaint/editor/JChemPaintEditor.java
@@ -54,216 +54,230 @@ import org.openscience.cdk.interfaces.IChemObject;
 import org.openscience.cdk.renderer.selection.LogicalSelection;
 import org.openscience.cdk.renderer.selection.LogicalSelection.Type;
 
-public class JChemPaintEditor extends EditorPart implements ISelectionListener{
+public class JChemPaintEditor extends EditorPart implements ISelectionListener {
 
-    Logger logger = Logger.getLogger( JChemPaintEditor.class );
+    Logger                 logger = Logger.getLogger( JChemPaintEditor.class );
 
     private JCPOutlinePage fOutlinePage;
 
-
-    ICDKMolecule model;
+    ICDKMolecule           model;
     JChemPaintEditorWidget widget;
-    IControllerModel c2dm;
-    SWTMouseEventRelay relay;
-    Menu menu;
-
+    IControllerModel       c2dm;
+    SWTMouseEventRelay     relay;
+    Menu                   menu;
 
     public JChemPaintEditorWidget getWidget() {
-    	return widget;
+
+        return widget;
     }
 
-	@Override
-	public void doSave(IProgressMonitor monitor) {
+    @Override
+    public void doSave( IProgressMonitor monitor ) {
 
-		try {
+        try {
 
-		        Activator.getDefault().getCDKManager().
-            saveMolecule( model,
-                          model.getResource().getLocationURI().toString(),
-                          true);
+            Activator.getDefault().getCDKManager()
+            .saveMolecule(
+                          model,
+                          model.getResource().getLocationURI()
+                          .toString(), true );
 
             widget.setDirty( false );
         } catch ( BioclipseException e ) {
             monitor.isCanceled();
-            logger.debug( "Failed to save file: "+e.getMessage() );
+            logger.debug( "Failed to save file: " + e.getMessage() );
         } catch ( CDKException e ) {
             monitor.isCanceled();
-            logger.debug( "Failed to save file: "+e.getMessage() );
+            logger.debug( "Failed to save file: " + e.getMessage() );
         } catch ( CoreException e ) {
             monitor.isCanceled();
-            logger.debug( "Failed to save file: "+e.getMessage() );
+            logger.debug( "Failed to save file: " + e.getMessage() );
         }
-	}
-
-	@Override
-	public void doSaveAs() {
-	    SaveAsDialog saveAsDialog = new SaveAsDialog(this.getSite().getShell());
-	    if(model.getResource() instanceof IFile)
-	        saveAsDialog.setOriginalFile( (IFile)model.getResource() );
-	    int result = saveAsDialog.open();
-	    if(result == 1) {
-	        logger.debug( "SaveAs canceled.");
-	        return;
-	    }
-
-	    IPath path = saveAsDialog.getResult();
-	    try {
-
-	        Activator.getDefault().getCDKManager().
-          saveMolecule( model,
-                        path.toPortableString(),
-                        true);
-	    } catch ( BioclipseException e ) {
-	        logger.warn( "Failed to save molecule. " + e.getMessage());
-	    } catch ( CDKException e ) {
-	        logger.warn( "Failed to save molecule. " + e.getMessage());
-	    } catch ( CoreException e ) {
-	        logger.warn( "Failed to save molecule. " + e.getMessage());
-	    }
-	    widget.setDirty( false );
-	}
-
-	@Override
-	public void init(IEditorSite site, IEditorInput input)
-	                                                throws PartInitException {
-
-	    setSite(site);
-	    setInput(input);
-	    ICDKMolecule cModel = (ICDKMolecule)input.getAdapter( ICDKMolecule.class );
-	    if(cModel == null){
-	        IFile file = (IFile) input.getAdapter(IFile.class);
-	        if(file != null)
-	            cModel=(ICDKMolecule)  file.getAdapter(ICDKMolecule.class);
-	    }
-	    if(cModel != null ){
-
-	        setPartName(input.getName());
-	        model=cModel;
-	    }
-	}
-
-	@Override
-	public boolean isDirty() {
-		return widget.getDirty();
-	}
-
-	@Override
-	public boolean isSaveAsAllowed() {
-		return true;
-	}
-
-	@Override
-	public void createPartControl(Composite parent) {
-	    //  create widget
-		widget=new JChemPaintEditorWidget(parent,SWT.NONE) {
-		    @Override
-		    public void setDirty( boolean dirty ) {
-		        super.setDirty( dirty );
-		        firePropertyChange( IEditorPart.PROP_DIRTY );
-		    }
-		};
-		IAtomContainer atomContainer=null;
-		if(model!=null)
-		    atomContainer=model.getAtomContainer();
-
-
-		MenuManager menuMgr = new MenuManager();
-	  menuMgr.add( new GroupMarker(IWorkbenchActionConstants.MB_ADDITIONS));
-	  getSite().registerContextMenu( menuMgr, widget);
-	  //getSite().registerContextMenu( "net.bioclipse.cdk.ui.editors.jchempaint.menu",
-	    //                             menuMgr, widget);
-
-	  //Control control = lViewer.getControl();
-	  menu = menuMgr.createContextMenu(widget);
-	  widget.setMenu(menu);
-
-
-		// setup hub
-		getSite().setSelectionProvider( widget );
-		getSite().getPage().addSelectionListener(this);
-		widget.setInput( model );
-
-		parent.addDisposeListener( new DisposeListener () {
-
-            public void widgetDisposed( DisposeEvent e ) {
-                disposeControll( e );
-            }
-		});
-
-	}
+    }
 
     @Override
-	public void setFocus() {
-		widget.setFocus();
-	}
+    public void doSaveAs() {
+
+        SaveAsDialog saveAsDialog = new SaveAsDialog( this.getSite().getShell() );
+        if ( model.getResource() instanceof IFile )
+            saveAsDialog.setOriginalFile( (IFile) model.getResource() );
+        int result = saveAsDialog.open();
+        if ( result == 1 ) {
+            logger.debug( "SaveAs canceled." );
+            return;
+        }
+
+        IPath path = saveAsDialog.getResult();
+        try {
+
+            Activator.getDefault().getCDKManager()
+            .saveMolecule( model, path.toPortableString(), true );
+        } catch ( BioclipseException e ) {
+            logger.warn( "Failed to save molecule. " + e.getMessage() );
+        } catch ( CDKException e ) {
+            logger.warn( "Failed to save molecule. " + e.getMessage() );
+        } catch ( CoreException e ) {
+            logger.warn( "Failed to save molecule. " + e.getMessage() );
+        }
+        widget.setDirty( false );
+    }
+
+    @Override
+    public void init( IEditorSite site, IEditorInput input )
+                                       throws PartInitException {
+
+        setSite( site );
+        setInput( input );
+        ICDKMolecule cModel = (ICDKMolecule) input
+        .getAdapter( ICDKMolecule.class );
+        if ( cModel == null ) {
+            IFile file = (IFile) input.getAdapter( IFile.class );
+            if ( file != null )
+                cModel = (ICDKMolecule) file.getAdapter( ICDKMolecule.class );
+        }
+        if ( cModel != null ) {
+
+            setPartName( input.getName() );
+            model = cModel;
+        }
+    }
+
+    @Override
+    public boolean isDirty() {
+
+        return widget.getDirty();
+    }
+
+    @Override
+    public boolean isSaveAsAllowed() {
+
+        return true;
+    }
+
+    @Override
+    public void createPartControl( Composite parent ) {
+
+        // create widget
+        widget = new JChemPaintEditorWidget( parent, SWT.NONE ) {
+
+            @Override
+            public void setDirty( boolean dirty ) {
+
+                super.setDirty( dirty );
+                firePropertyChange( IEditorPart.PROP_DIRTY );
+            }
+        };
+
+        MenuManager menuMgr = new MenuManager();
+        menuMgr.add( new GroupMarker( IWorkbenchActionConstants.MB_ADDITIONS ) );
+        getSite().registerContextMenu( menuMgr, widget );
+        // getSite().registerContextMenu(
+        // "net.bioclipse.cdk.ui.editors.jchempaint.menu",
+        // menuMgr, widget);
+
+        // Control control = lViewer.getControl();
+        menu = menuMgr.createContextMenu( widget );
+        widget.setMenu( menu );
+
+        // setup hub
+        getSite().setSelectionProvider( widget );
+        getSite().getPage().addSelectionListener( this );
+        widget.setInput( model );
+
+        parent.addDisposeListener( new DisposeListener() {
+
+            public void widgetDisposed( DisposeEvent e ) {
+
+                disposeControll( e );
+            }
+        } );
+
+    }
+
+    @Override
+    public void setFocus() {
+
+        widget.setFocus();
+    }
 
     public ControllerHub getControllerHub() {
+
         return widget.getControllerHub();
     }
 
     public IControllerModel getControllerModel() {
+
         return c2dm;
     }
 
     public void update() {
+
         widget.redraw();
     }
 
     public void setInput( Object element ) {
+
         widget.setInput( element );
         widget.redraw();
     }
 
     public ICDKMolecule getCDKMolecule() {
+
         return model;
     }
 
     @SuppressWarnings("unchecked")
-    public Object getAdapter(Class adapter) {
-        if (IContentOutlinePage.class.equals(adapter)) {
-            if (fOutlinePage == null) {
-                fOutlinePage= new JCPOutlinePage(getEditorInput(), this);
+    public Object getAdapter( Class adapter ) {
+
+        if ( IContentOutlinePage.class.equals( adapter ) ) {
+            if ( fOutlinePage == null ) {
+                fOutlinePage = new JCPOutlinePage( getEditorInput(), this );
             }
             return fOutlinePage;
         }
-        if (IAtomContainer.class.equals( adapter )) {
+        if ( IAtomContainer.class.equals( adapter ) ) {
             return model.getAtomContainer();
         }
-        return super.getAdapter(adapter);
+        return super.getAdapter( adapter );
     }
 
     public void doAddAtom() {
 
         logger.debug( "Executing 'Add atom' action" );
     }
+
     public void doChageAtom() {
+
         logger.debug( "Executing 'Chage atom' action" );
     }
 
     public void selectionChanged( IWorkbenchPart part, ISelection selection ) {
-        if(part != null && part.equals( this )) return;
-        if(selection instanceof IStructuredSelection) {
+
+        if ( part != null && part.equals( this ) )
+            return;
+        if ( selection instanceof IStructuredSelection ) {
 
             IAtomContainer container = null;
 
-            for(Iterator<?> iter =((IStructuredSelection)selection).iterator();iter.hasNext();) {
+            for ( Iterator<?> iter = ((IStructuredSelection) selection)
+                    .iterator(); iter.hasNext(); ) {
                 Object c = iter.next();
-                if(! (c instanceof CDKChemObject)) {
+                if ( !(c instanceof CDKChemObject) ) {
                     continue;
                 }
 
-                IChemObject o = ((CDKChemObject)c).getChemobj();
-                if(container == null )
+                IChemObject o = ((CDKChemObject) c).getChemobj();
+                if ( container == null )
                     container = o.getBuilder().newAtomContainer();
-                if(o instanceof IAtom) {
-                    container.addAtom( (IAtom)o );
-                }else if( o instanceof IBond) {
-                    container.addBond( (IBond)o);
+                if ( o instanceof IAtom ) {
+                    container.addAtom( (IAtom) o );
+                } else if ( o instanceof IBond ) {
+                    container.addBond( (IBond) o );
                 }
             }
 
-            LogicalSelection sel = new LogicalSelection(Type.NONE);
-            if(container!= null) {
+            LogicalSelection sel = new LogicalSelection( Type.NONE );
+            if ( container != null ) {
                 sel.select( container );
                 widget.getRenderer2DModel().setSelection( sel );
             } else {
@@ -273,10 +287,12 @@ public class JChemPaintEditor extends EditorPart implements ISelectionListener{
         }
     }
 
-    private void disposeControll(DisposeEvent e) {
+    private void disposeControll( DisposeEvent e ) {
+
         // TODO remove regiistration?
-        //getSite().registerContextMenu( "net.bioclipse.cdk.ui.editors.jchempaint.menu",
-        //                               menuMgr, widget);
+        // getSite().registerContextMenu(
+        // "net.bioclipse.cdk.ui.editors.jchempaint.menu",
+        // menuMgr, widget);
 
         getSite().setSelectionProvider( null );
         getSite().getPage().removeSelectionListener( this );
