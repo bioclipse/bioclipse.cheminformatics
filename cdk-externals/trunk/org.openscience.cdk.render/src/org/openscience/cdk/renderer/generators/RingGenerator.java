e61e12b4cac2366a8dc3b28365cb41d6bf344be0
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-02-17 11:12:54 +0000

    Reset the isAromatic var when checking the bonds
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@8871 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- cdk-externals/trunk/org.openscience.cdk.render/src/org/openscience/cdk/renderer/generators/RingGenerator.java
+++ cdk-externals/trunk/org.openscience.cdk.render/src/org/openscience/cdk/renderer/generators/RingGenerator.java
@@ -93,6 +93,7 @@ public class RingGenerator extends BasicBondGenerator {
 			}
 		}
 		if (!isAromatic) {
+		    isAromatic = true;
 			for (IBond b : ring.bonds()) {
 				if (!b.getFlag(ISAROMATIC)) {
 					return false;
