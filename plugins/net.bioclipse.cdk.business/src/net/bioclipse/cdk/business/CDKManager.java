6757d5010c48aa4ec3004462b2868b324afffc8b
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-02-20 07:28:21 +0000

    Replaced use of the deprecated StringBufferInputStream with the ByteArrayInputStream
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@8931 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk.business/src/net/bioclipse/cdk/business/CDKManager.java
+++ plugins/net.bioclipse.cdk.business/src/net/bioclipse/cdk/business/CDKManager.java
@@ -20,7 +20,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.io.StringBufferInputStream;
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.InvocationTargetException;
@@ -1518,7 +1517,8 @@ public class CDKManager implements ICDKManager {
 
   	            CMLReader reader
   	                = new CMLReader(
-  	                    new StringBufferInputStream(entries[i].getCML()) );
+  	                    new ByteArrayInputStream(entries[i].getCML().getBytes())
+  	            );
 
   	            IAtomContainer ac
   	                = ((IChemFile)reader.read(
@@ -1534,7 +1534,7 @@ public class CDKManager implements ICDKManager {
   	            sb.append("$$$$"+System.getProperty("line.separator"));
   	        }
 
-  	        file.create( new StringBufferInputStream(sb.toString()),
+  	        file.create( new ByteArrayInputStream(sb.toString().getBytes()),
   	                     false,
   	                     monitor );
   	        monitor.worked(ticks);
