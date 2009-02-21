2210159833d0e41cc34318d8a7deba9d2c9a4a13
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-02-21 15:12:28 +0000

    Ported the search functionality from BC1 to BC2
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@8950 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.pubchem/src/net/bioclipse/pubchem/business/PubChemManager.java
+++ plugins/net.bioclipse.pubchem/src/net/bioclipse/pubchem/business/PubChemManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009  Egon Willighagen <egonw@users.sf.net>
+ * Copyright (c) 2006-2009  Egon Willighagen <egonw@users.sf.net>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -14,11 +14,17 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.PatternSyntaxException;
 
 import net.bioclipse.core.ResourcePathTransformer;
 import net.bioclipse.core.business.BioclipseException;
+import nu.xom.Builder;
+import nu.xom.Document;
+import nu.xom.Nodes;
+import nu.xom.ParsingException;
+import nu.xom.ValidityException;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
@@ -27,8 +33,11 @@ import org.eclipse.core.runtime.NullProgressMonitor;
 
 public class PubChemManager implements IPubChemManager {
 
+    private final static String EUTILS_URL_BASE = "http://www.ncbi.nlm.nih.gov/entrez/eutils";
     private final static String PUBCHEM_URL_BASE = "http://pubchem.ncbi.nlm.nih.gov/";
-    
+
+    private final static String TOOL = "bioclipse.net";
+
     public String getNamespace() {
         return "pubchem";
     }
@@ -72,16 +81,73 @@ public class PubChemManager implements IPubChemManager {
         monitor.done();
     }
 
+    private String replaceSpaces(String molecule2) {
+        StringBuffer result = new StringBuffer();
+        for (int i=0; i<molecule2.length(); i++) {
+            if (Character.isWhitespace(molecule2.charAt(i))) {
+                result.append("+");
+            } else {
+                result.append(molecule2.charAt(i));
+            }
+        }
+        return result.toString();
+    }
+
     public List<Integer> search(String query) throws IOException,
             BioclipseException, CoreException {
-        // TODO Auto-generated method stub
-        return null;
+        return search(query, null);
     }
 
     public List<Integer> search(String query, IProgressMonitor monitor)
             throws IOException, BioclipseException, CoreException {
-        // TODO Auto-generated method stub
-        return null;
+        if (monitor == null) {
+            monitor = new NullProgressMonitor();
+        }
+
+        List<Integer> results = new ArrayList<Integer>();
+        monitor.beginTask("Searching PubChem for '" + query + "'...", 1);
+
+        String db = "pccompound";
+        query = replaceSpaces(query);
+
+        String esearch = EUTILS_URL_BASE + "/esearch.fcgi?" +
+            "db=" + db + "&retmax=50&usehistory=y&tool=" + TOOL + "&term=" + query;
+
+        URL queryURL = new URL(esearch);
+        URLConnection connection = queryURL.openConnection();
+
+        monitor.subTask("Processing search results...");
+        Builder parser = new Builder();
+        Document doc;
+        try {
+            doc = parser.build(connection.getInputStream());
+            Nodes countNodes = doc.query("/eSearchResult/Count");
+            if (countNodes.size() > 0) {
+                System.out.println(countNodes.get(0).toString());
+            } else {
+                monitor.done();
+                return results;
+            }
+
+            Nodes cidNodes = doc.query("/eSearchResult/IdList/Id");
+
+            int max = cidNodes.size();
+            if (max > 15) max = 15;
+
+            for (int cidCount=0; cidCount<max; cidCount++) {
+                String cidStr = cidNodes.get(cidCount).getValue();
+                int cid = Integer.parseInt(cidStr);
+                results.add(cid);
+            }
+        } catch (ValidityException e) {
+            e.printStackTrace();
+        } catch (ParsingException e) {
+            e.printStackTrace();
+        } finally {
+            monitor.done();
+        }
+
+        return results;
     }
 
 }
