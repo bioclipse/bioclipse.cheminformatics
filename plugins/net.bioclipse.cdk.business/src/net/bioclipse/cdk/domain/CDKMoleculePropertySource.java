ce1b03c33685cab47bba5e8a7471cbb2cd211832
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-02-24 16:55:31 +0000

    Added IChemObject.properties to the Properties view (fixes #519)
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9038 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk.business/src/net/bioclipse/cdk/domain/CDKMoleculePropertySource.java
+++ plugins/net.bioclipse.cdk.business/src/net/bioclipse/cdk/domain/CDKMoleculePropertySource.java
@@ -13,6 +13,7 @@ package net.bioclipse.cdk.domain;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import net.bioclipse.cdk.business.Activator;
 import net.bioclipse.cdk.business.ICDKManager;
@@ -26,10 +27,10 @@ import org.eclipse.ui.views.properties.IPropertyDescriptor;
 import org.eclipse.ui.views.properties.PropertyDescriptor;
 import org.eclipse.ui.views.properties.TextPropertyDescriptor;
 import org.openscience.cdk.geometry.GeometryTools;
+import org.openscience.cdk.interfaces.IChemObject;
 
 public class CDKMoleculePropertySource extends BioObjectPropertySource {
 
-    protected static final String PROPERTY_SMILES = "SMILES";
     protected static final String PROPERTY_HAS2D = "Has 2D Coords";
     protected static final String PROPERTY_HAS3D = "Has 3D Coords";
     protected static final String PROPERTY_FORMAT = "Molecular Format";
@@ -38,8 +39,6 @@ public class CDKMoleculePropertySource extends BioObjectPropertySource {
 
     private final Object cdkPropertiesTable[][] =
     {
-        { PROPERTY_SMILES,
-            new TextPropertyDescriptor(PROPERTY_SMILES,PROPERTY_SMILES)},
         { PROPERTY_HAS2D,
             new TextPropertyDescriptor(PROPERTY_HAS2D,PROPERTY_HAS2D)},
         { PROPERTY_HAS3D,
@@ -60,63 +59,78 @@ public class CDKMoleculePropertySource extends BioObjectPropertySource {
         super(item);
         cdkMol = item;
         
-        cdkProperties = new ArrayList<IPropertyDescriptor>();
-        cdkValueMap = new HashMap<String, Object>();
+        cdkProperties = setupProperties(item.getAtomContainer());
+        cdkValueMap = getPropertyValues(item);
+    }
 
+    /**
+     * @param item
+     */
+    private HashMap<String, Object> getPropertyValues(CDKMolecule item) {
+        HashMap<String, Object> valueMap = new HashMap<String, Object>();
+        valueMap.put(
+            PROPERTY_HAS2D,
+            GeometryTools.has2DCoordinates(item.getAtomContainer()) ?
+                "yes" : "no"
+        );
+        valueMap.put(
+            PROPERTY_HAS3D,
+            GeometryTools.has3DCoordinates(item.getAtomContainer()) ?
+                "yes" : "no"
+        );
+        ICDKManager cdk = Activator.getDefault().getCDKManager();
+        valueMap.put(
+            PROPERTY_FORMULA, cdk.molecularFormula(item)
+        );
+        try {
+            valueMap.put(
+                PROPERTY_MASS, cdk.calculateMass(item)
+            );
+        } catch (BioclipseException e) {
+            valueMap.put(PROPERTY_MASS, "N/A");
+        }
+        IResource resource = item.getResource();
+        if (resource instanceof IFile) {
+            IFile fileRes = (IFile)resource;
+            try {
+                valueMap.put(
+                    PROPERTY_FORMAT, fileRes.getContentDescription().getContentType().getName()
+                );
+            } catch (CoreException e) {
+                valueMap.put(PROPERTY_FORMAT, "error");
+            }
+        } else {
+            valueMap.put(PROPERTY_FORMAT, "N/A");
+        }
+        // IChemObject.getProperties()
+        Map<Object,Object> objectProps = item.getAtomContainer().getProperties();
+        for (Object propKey : objectProps.keySet()) {
+            String label = ""+propKey;
+            valueMap.put(label, ""+objectProps.get(propKey));
+        }
+        return valueMap;
+    }
+
+    private ArrayList<IPropertyDescriptor> setupProperties(IChemObject object) {
+        ArrayList<IPropertyDescriptor> cdkProperties =
+            new ArrayList<IPropertyDescriptor>();
+        // default properties
         for (int i=0;i<cdkPropertiesTable.length;i++) {
             PropertyDescriptor descriptor;
             descriptor = (PropertyDescriptor)cdkPropertiesTable[i][1];
             descriptor.setCategory("General");
             cdkProperties.add(descriptor);
         }
-
-        String smiles = null;
-        try {
-            smiles = cdkMol.getSMILES();
-        } catch (BioclipseException e) {
-            // FIXME: use a logger
-            e.printStackTrace();
+        // IChemObject.getProperties()
+        Map<Object,Object> objectProps = object.getProperties();
+        for (Object propKey : objectProps.keySet()) {
+            PropertyDescriptor descriptor;
+            String label = ""+propKey;
+            descriptor = new TextPropertyDescriptor(label,label);
+            descriptor.setCategory("Molecular Properties");
+            cdkProperties.add(descriptor);
         }
-        cdkValueMap.put(
-            PROPERTY_SMILES,
-            smiles == null ? "N/A" : smiles
-        );
-
-        cdkValueMap.put(
-            PROPERTY_HAS2D,
-            GeometryTools.has2DCoordinates(item.getAtomContainer()) ?
-                "yes" : "no"
-        );
-        cdkValueMap.put(
-            PROPERTY_HAS3D,
-            GeometryTools.has3DCoordinates(item.getAtomContainer()) ?
-                "yes" : "no"
-        );
-        ICDKManager cdk = Activator.getDefault().getCDKManager();
-        cdkValueMap.put(
-            PROPERTY_FORMULA, cdk.molecularFormula(item)
-        );
-        try {
-            cdkValueMap.put(
-                PROPERTY_MASS, cdk.calculateMass(item)
-            );
-        } catch (BioclipseException e) {
-            cdkValueMap.put(PROPERTY_MASS, "N/A");
-        }
-        IResource resource = item.getResource();
-        if (resource instanceof IFile) {
-            IFile fileRes = (IFile)resource;
-            try {
-                cdkValueMap.put(
-                    PROPERTY_FORMAT, fileRes.getContentDescription().getContentType().getName()
-                );
-            } catch (CoreException e) {
-                cdkValueMap.put(PROPERTY_FORMAT, "error");
-            }
-        } else {
-            cdkValueMap.put(PROPERTY_FORMAT, "N/A");
-        }
-
+        return cdkProperties;
     }
 
     public IPropertyDescriptor[] getPropertyDescriptors() {
