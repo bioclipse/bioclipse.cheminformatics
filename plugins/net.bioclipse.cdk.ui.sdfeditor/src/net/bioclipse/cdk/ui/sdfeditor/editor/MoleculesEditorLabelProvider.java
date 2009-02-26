78239f22bb3179b290a74c0cd1400dbe0a474b75
goglepox <goglepox@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-02-26 14:11:19 +0000

    Refactored for better overview and limited object creation
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9095 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk.ui.sdfeditor/src/net/bioclipse/cdk/ui/sdfeditor/editor/MoleculesEditorLabelProvider.java
+++ plugins/net.bioclipse.cdk.ui.sdfeditor/src/net/bioclipse/cdk/ui/sdfeditor/editor/MoleculesEditorLabelProvider.java
@@ -12,27 +12,24 @@
  ******************************************************************************/
 package net.bioclipse.cdk.ui.sdfeditor.editor;
 
-import java.awt.Dimension;
 import java.awt.geom.Rectangle2D;
+import java.awt.geom.Rectangle2D.Double;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.WeakHashMap;
 
 import javax.vecmath.Point2d;
 import javax.vecmath.Point3d;
 
 import net.bioclipse.cdk.business.Activator;
-import net.bioclipse.cdk.domain.CDKMolecule;
 import net.bioclipse.cdk.domain.ICDKMolecule;
 import net.bioclipse.cdk.domain.SDFElement;
 import net.bioclipse.cdk.jchempaint.view.SWTFontManager;
 import net.bioclipse.cdk.jchempaint.view.SWTRenderer;
 
 import org.apache.log4j.Logger;
+import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.jface.viewers.ITableLabelProvider;
@@ -42,15 +39,14 @@ import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
-import org.eclipse.swt.graphics.Transform;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.progress.PendingUpdateAdapter;
-import org.openscience.cdk.Molecule;
 import org.openscience.cdk.geometry.GeometryTools;
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
-import org.openscience.cdk.interfaces.IMolecule;
 import org.openscience.cdk.renderer.Renderer;
+import org.openscience.cdk.renderer.RendererModel;
 import org.openscience.cdk.renderer.font.IFontManager;
 import org.openscience.cdk.renderer.generators.BasicAtomGenerator;
 import org.openscience.cdk.renderer.generators.BasicBondGenerator;
@@ -58,152 +54,140 @@ import org.openscience.cdk.renderer.generators.HighlightAtomGenerator;
 import org.openscience.cdk.renderer.generators.HighlightBondGenerator;
 import org.openscience.cdk.renderer.generators.IGenerator;
 import org.openscience.cdk.renderer.generators.RingGenerator;
-import org.openscience.cdk.renderer.visitor.IDrawVisitor;
-import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 
 public class MoleculesEditorLabelProvider implements ITableLabelProvider{
 
     public Logger logger = Logger.getLogger(MoleculesEditorLabelProvider.class );
     public List<String>                          propertyHeaders;
-    public Renderer                           renderer;
-    private IDrawVisitor drawVisitor;
+    public Renderer renderer;
     public int imageWidth;
     private  IRenderer2DConfigurator renderer2DConfigurator;
+    Color greenScreen;
+    Color generatedColor;
+    Font generatedFont;
 
     Collection<ILabelProviderListener> listeners =
      new HashSet<ILabelProviderListener>();
+    private Double imageBounds;
+    private Image tempImage;
+    private Rectangle imageBoundsSWT;
 
     public MoleculesEditorLabelProvider(int width) {
+
         imageWidth= width;
         setupRenderer();
-
     }
-    
+
     public IRenderer2DConfigurator getRenderer2DConfigurator() {
         return renderer2DConfigurator;
     }
+
     public void setRenderer2DConfigurator(
                              IRenderer2DConfigurator renderer2DConfigurator ) {
         this.renderer2DConfigurator = renderer2DConfigurator;
     }
-    
-    
+
     public void setPropertyHeaders(List<String> headers){
         propertyHeaders = headers;
     }
     private void setupRenderer() {
 
-
         IFontManager fontManager = new SWTFontManager(Display.getCurrent());
-        
+
         List<IGenerator> generators = new ArrayList<IGenerator>();
         generators.add(new BasicBondGenerator());
         generators.add(new BasicAtomGenerator());
         generators.add(new RingGenerator());
         generators.add(new HighlightAtomGenerator());
         generators.add(new HighlightBondGenerator());
-        
+
         renderer = new Renderer(generators, fontManager);
 
+        RendererModel rModel = renderer.getRenderer2DModel();
+        rModel.setDrawNumbers( false );
+        rModel.setIsCompact( true );
+//        rModel.setUseAntiAliasing(true );
 
-        renderer.getRenderer2DModel().setDrawNumbers( false );
-        renderer.getRenderer2DModel().setIsCompact( true );
-        //renderer.getRenderer2DModel().setBondWidth( 15 );
-        renderer.getRenderer2DModel().setDrawNumbers( false );
-        //renderer.getRenderer2DModel().setBondDistance( 1 );
-        renderer.getRenderer2DModel().setUseAntiAliasing(true );
+        rModel.setShowExplicitHydrogens( false );
+        rModel.setBackColor( new java.awt.Color(252,253,254));
+        rModel.setFitToScreen( true );
+
+        greenScreen = new Color(Display.getCurrent(), 252, 253, 254);
+        generatedColor = new Color(Display.getCurrent(),200,100,100);
+        generatedFont =new Font(Display.getCurrent(),"Arial",16,SWT.BOLD);
+        imageBounds = new Rectangle2D.Double( 0, 0, imageWidth, imageWidth );
+        tempImage = new Image( Display.getDefault(),
+                               imageWidth,
+                               imageWidth );
+        imageBoundsSWT = tempImage.getBounds();
+
+    }
+
+    private boolean retriveAtomContainer(IAdaptable element,IAtomContainer[] result) {
+
+        Assert.isTrue( result!=null && result.length >0);
+        ICDKMolecule mol = (ICDKMolecule)element.getAdapter( ICDKMolecule.class);
+        if(mol == null) return false;
+
+        // If no 2D coordinates
+        if ( !GeometryTools.has2DCoordinates( mol.getAtomContainer() ) ) {
+            // Test if 3D coordinates
+            IAtomContainer generatedAC = null;
+            try {
+
+                generatedAC = ((ICDKMolecule)Activator.getDefault()
+                        .getCDKManager()
+                        .generate2dCoordinates( mol ))
+                        .getAtomContainer();
+                result[0] = generatedAC;
+            } catch ( Exception e ) {
+                logger.info( "Failed to generate 2D-coordinates" );
+            }
+            return true;
+        }else
+            result[0]= mol.getAtomContainer();
+        return false;
     }
 
     public Image getColumnImage( Object element, int columnIndex ) {
         boolean generated = false;
 
         if ( columnIndex == 1 && element instanceof IAdaptable ) {
-            ICDKMolecule mol =
-                    (ICDKMolecule) ((IAdaptable) element)
-                            .getAdapter( ICDKMolecule.class );
-            if ( mol == null )
-                return null;
-            IAtomContainer drawMolecule = mol.getAtomContainer();
 
-            // If no 2D coordinates
-            if ( GeometryTools.has2DCoordinates( drawMolecule ) == false ) {
-                // Test if 3D coordinates
-                drawMolecule = null;
-                if(drawMolecule == null) {
-                    try {
+            IAtomContainer[] acArray= new IAtomContainer[1];
+            generated = retriveAtomContainer( (IAdaptable ) element,
+                                              acArray);
+            if(acArray[0] == null) return null;
 
-                        drawMolecule = ((ICDKMolecule)Activator.getDefault()
-                                .getCDKManager()
-                                .generate2dCoordinates( mol ))
-                                .getAtomContainer();
-                    } catch ( Exception e ) {
-                        logger.info( "Failed to generate 2D-coordinates" );
 
-                        return null;
-                    }
-                }
-                generated = true;
+//            if (renderer2DConfigurator!=null){
+//                renderer2DConfigurator.configure( renderer.getRenderer2DModel(),
+//                                                  mol );
+//            }
+
+            GC gc= new GC( tempImage );
+
+
+            gc.setBackground( generatedColor );
+            gc.fillRectangle( imageBoundsSWT );
+
+            if(generated) {
+                gc.setFont( generatedFont );
+                int h = imageBoundsSWT.height-gc.getFontMetrics().getHeight();
+                gc.setForeground( generatedColor);
+                gc.drawText( "Generated", 0, h );
             }
 
+            SWTRenderer drawVisitor= new SWTRenderer(gc);
+            renderer.paintMolecule( acArray[0],
+                                    drawVisitor,
+                                    imageBounds,
+                                    true );
+            gc.dispose();
 
-
-            // renderer.getRenderer2DModel().setRenderingCoordinates(
-            // coordinates);
-
-            renderer.getRenderer2DModel()
-                    .setShowExplicitHydrogens( false );
-
-            Image image;
-
-            renderer.getRenderer2DModel().setBackColor(new java.awt.Color(252,253,254));
-//            renderer.getRenderer2DModel().setBackColor( java.awt.Color.CYAN );
-            renderer.getRenderer2DModel().setUseAntiAliasing( true );
-            renderer.getRenderer2DModel().setHighlightDistance( 10 );
-            renderer.getRenderer2DModel().setFitToScreen( true );
-
-            if (renderer2DConfigurator!=null){
-                renderer2DConfigurator.configure( renderer.getRenderer2DModel(), mol );
-            }
-            Color greenScreen = new Color(Display.getCurrent(), 252, 253, 254);
-
-                image =
-                    new Image( Display.getDefault(),
-                               imageWidth,
-                               imageWidth );
-                GC gc= new GC( image );
-
-
-                gc.setBackground( greenScreen );
-                gc.fillRectangle( image.getBounds() );
-
-
-                Color generatedColor = new Color(gc.getDevice(),200,100,100);
-                Font generatedFont =new Font(gc.getDevice(),"Arial",16,SWT.BOLD);
-                if(generated) {
-                    gc.setFont( generatedFont );
-                    int h = image.getBounds().height-gc.getFontMetrics().getHeight();
-                    gc.setForeground( generatedColor);
-                    gc.drawText( "Generated", 0, h );
-
-                }
-
-                SWTRenderer drawVisitor= new SWTRenderer(gc);
-                renderer.paintMolecule( drawMolecule,
-                                        drawVisitor,
-                                        new Rectangle2D.Double( 0, 0,
-                                                                imageWidth,
-                                                                imageWidth ),
-                                        true );
-
-                gc.dispose();
-                generatedColor.dispose();
-
-            ImageData imageData = image.getImageData();
+            ImageData imageData = tempImage.getImageData();
             imageData.transparentPixel = imageData.palette.getPixel(greenScreen
-            .getRGB());
-
-            greenScreen.dispose();
-            image.dispose();
+                                                                    .getRGB());
             return new Image(Display.getDefault(),imageData);
         }
         return null;
@@ -256,7 +240,10 @@ public class MoleculesEditorLabelProvider implements ITableLabelProvider{
     }
 
     public void dispose() {
-
+        greenScreen.dispose();
+        generatedColor.dispose();
+        generatedFont.dispose();
+        tempImage.dispose();
         listeners.clear();
     }
 
