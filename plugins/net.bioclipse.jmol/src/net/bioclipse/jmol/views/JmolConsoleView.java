363ee24ea4077994c4b9af8471958f78d3e897d2
carl_masak <carl_masak@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-03-09 10:59:53 +0000

    moved JmolConsoleView.java from n.b.scripting.ui to n.b.jmol
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9253 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.jmol/src/net/bioclipse/jmol/views/JmolConsoleView.java
@@ -0,0 +1,13 @@
+package net.bioclipse.jmol.views;
+
+import net.bioclipse.scripting.ui.views.ScriptingConsoleView;
+
+
+public class JmolConsoleView extends ScriptingConsoleView {
+
+    @Override
+    protected String executeCommand( String command ) {
+        return "Not implemented yet";
+    }
+
+}
