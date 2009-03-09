ebe32fd495c64f22ea13aa27e743071948fbf1c5
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-03-09 14:43:08 +0000

    Added functionality for determining JCP editor from an ICDKMolecule according to bug #654.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9267 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.cdk.jchempaint/src/net/bioclipse/cdk/jchempaint/describer/JCPEditorDescriber.java
@@ -0,0 +1,34 @@
+package net.bioclipse.cdk.jchempaint.describer;
+
+import net.bioclipse.cdk.business.Activator;
+import net.bioclipse.cdk.business.ICDKManager;
+import net.bioclipse.cdk.domain.ICDKMolecule;
+import net.bioclipse.core.business.BioclipseException;
+import net.bioclipse.core.domain.IBioObject;
+import net.bioclipse.ui.business.describer.IBioObjectDescriber;
+
+
+public class JCPEditorDescriber implements IBioObjectDescriber {
+
+    ICDKManager cdk;
+
+    public JCPEditorDescriber() {
+
+        cdk=Activator.getDefault().getCDKManager();
+    }
+
+    public String getPreferredEditorID( IBioObject object ) {
+        if ( object instanceof ICDKMolecule ) {
+            ICDKMolecule cdkmol = (ICDKMolecule) object;
+            try {
+                if (cdk.has2d( cdkmol )){
+                    return "net.bioclipse.cdk.ui.editors.jchempaint";
+                }
+            } catch ( BioclipseException e ) {
+            }
+        }
+
+        return null;
+    }
+
+}
