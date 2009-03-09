00b1a79af77b02888deb2efa258b201ec36e862f
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-03-09 14:08:15 +0000

    Added plugin ID and updated Javadoc.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9264 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.jmol/src/net/bioclipse/jmol/editors/JmolEditor.java
+++ plugins/net.bioclipse.jmol/src/net/bioclipse/jmol/editors/JmolEditor.java
@@ -76,15 +76,11 @@ import org.eclipse.ui.ide.IDE;
 import org.jmol.modelset.Model;
 
 /**
- * An example showing how to create a multi-page editor.
- * This example has 3 pages:
- * <ul>
- * <li>page 0 contains a nested text editor.
- * <li>page 1 allows you to change the font used in page 2
- * <li>page 2 shows the words in page 0 in sorted order
- * </ul>
+ * A Multi Page Editor with Jmol embedded on foremost page.
  */
 public class JmolEditor extends MultiPageEditorPart implements IResourceChangeListener, IAdaptable, ISelectionListener, ISelectionProvider{
+
+    public static final String EDITOR_ID="net.bioclipse.jmol.editors.JmolEditor";
 
     private static final Logger logger = Logger.getLogger(JmolEditor.class);
 
