45da8aaaf0a55b90880bf31ae3fa5ba2a4206c2f
shk3 <shk3@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-04-08 17:27:30 +0000

    All the possibilities are now properly handled with respect to reset. More classes, but cleaner code.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9717 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.cdk.ui/src/net/bioclipse/cdk/ui/handlers/Create2dHandlerWithReset.java
@@ -0,0 +1,137 @@
+/*******************************************************************************
+ * Copyright (c) 2008 The Bioclipse Project and others. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html Contributors:
+ * Stefan Kuhn
+ ******************************************************************************/
+package net.bioclipse.cdk.ui.handlers;
+
+import net.bioclipse.cdk.business.Activator;
+import net.bioclipse.cdk.domain.ICDKMolecule;
+import net.bioclipse.core.util.LogUtils;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.dialogs.SaveAsDialog;
+import org.openscience.cdk.interfaces.IAtom;
+
+/**
+ * A handler class for a Generate 2D Coordinates menu item
+ */
+public class Create2dHandlerWithReset extends AbstractHandler {
+
+    private static final Logger logger =
+                                               Logger
+                                                       .getLogger( Create2dHandlerWithReset.class );
+    public int                  answer;
+
+    public Object execute( ExecutionEvent event ) throws ExecutionException {
+        doCreation(true,false);
+        return null;
+    }
+    
+    /**
+     * This method creates the coordinates and saves the file on the selection.
+     * 
+     * @param withReset If true, the other set of coordinates is set to null. This should be used on mol files, since these can only hold one set (3d or 2d).
+     * @param make3D true = 3d is generated, false = 2d is generated.
+     */
+    public static void doCreation(boolean withReset, boolean make3D){
+        ISelection sel =
+            PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+                    .getSelectionService().getSelection();
+      if ( !sel.isEmpty() ) {
+          if ( sel instanceof IStructuredSelection ) {
+              IStructuredSelection ssel = (IStructuredSelection) sel;
+              ICDKMolecule mol;
+              try {
+                  mol =
+                          Activator.getDefault().getCDKManager()
+                                  .loadMolecule(
+                                                 (IFile) ssel
+                                                         .getFirstElement(), new NullProgressMonitor() );
+                  if(make3D){
+                      mol =
+                          (ICDKMolecule) Activator.getDefault()
+                                  .getCDKManager()
+                                  .generate3dCoordinates( mol );                    
+                  }else{
+                    mol =
+                            (ICDKMolecule) Activator.getDefault()
+                                    .getCDKManager()
+                                    .generate2dCoordinates( mol );
+                  }
+                  if(withReset){
+                    //we set the other coordinates to null, since when writing out, they might override
+                    for(IAtom atom : mol.getAtomContainer().atoms()){
+                        if(make3D)
+                            atom.setPoint2d( null );
+                        else
+                            atom.setPoint3d( null );
+                    }
+                  }
+              } catch ( Exception e ) {
+                  LogUtils.handleException( e, logger );
+                  return;
+              }
+              MessageBox mb =
+                      new MessageBox( new Shell(), SWT.YES | SWT.NO | SWT.CANCEL
+                                                   | SWT.ICON_QUESTION );
+              mb.setText( "Change file" );
+              mb.setMessage( "Do you want to write the "+ (make3D ? "3D" : "2D")+ " coordinates into the existing file? If no, a new one will be created." );
+              int val = mb.open();
+              if ( val == SWT.YES ) {
+                  try {
+                      Activator.getDefault().getCDKManager()
+                              .saveMolecule(
+                                             mol,
+                                             (IFile) ssel.getFirstElement(),
+                                             true);
+                  } catch ( Exception e ) {
+                      throw new RuntimeException( e.getMessage() );
+                  }
+              } else if ( val == SWT.NO ){
+                  SaveAsDialog dialog = new SaveAsDialog( new Shell() );
+                  dialog.setOriginalFile( (IFile) ssel.getFirstElement() );
+                  int saveasreturn = dialog.open();
+                  IPath result = dialog.getResult();
+                  if ( saveasreturn != SaveAsDialog.CANCEL ) {
+                      if ( dialog.getResult().getFileExtension() == null )
+                          result =
+                                  result.addFileExtension( ((IFile) ssel
+                                          .getFirstElement())
+                                          .getFileExtension() );
+                      try {
+                          Activator
+                                  .getDefault()
+                                  .getCDKManager()
+                                  .saveMolecule(
+                                                 mol,
+                                                 ((IFile) ssel
+                                                         .getFirstElement())
+                                                         .getWorkspace()
+                                                         .getRoot()
+                                                         .getFile(
+                                                                   result ),
+                                                 true );
+                      } catch ( Exception e ) {
+                          throw new RuntimeException( e.getMessage() );
+                      }
+                  }
+              }
+          }
+      }
+    }
+}
---
+++ plugins/net.bioclipse.cdk.ui/src/net/bioclipse/cdk/ui/handlers/Create3dHandlerWithReset.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2008 The Bioclipse Project and others. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html Contributors:
+ * Stefan Kuhn
+ ******************************************************************************/
+package net.bioclipse.cdk.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+/**
+ * A handler class for a Generate 3D Coordinates menu item
+ */
+public class Create3dHandlerWithReset extends AbstractHandler {
+
+    public int                  answer;
+
+    public Object execute( ExecutionEvent event ) throws ExecutionException {
+        Create2dHandlerWithReset.doCreation(true,true);
+        return null;
+    }
+
+}
