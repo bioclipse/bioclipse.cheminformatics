bioclipse/bioclipse.cheminformatics

commit 2860d7226f157b15e5c8b94d162da8c758c93b46

Removed fit to screen from editor widget
and added setScale() when model is changed
Fixes bug #808

git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9847 fcb5ba71-d80d-0410-8237-ba3920747fcc

plugins/net.bioclipse.cdk.jchempaint/src/net/bioclipse/cdk/jchempaint/widgets/JChemPaintEditorWidget.java
@@ -306,23 +306,10 @@ public class JChemPaintEditorWidget extends JChemPaintWidget
             SWTRenderer visitor = new SWTRenderer(gc);
             Renderer renderer = getRenderer();
 
-            if (isNew) {
-                renderer.setScale(model);
-                java.awt.Rectangle diagramBounds =
-                    renderer.calculateDiagramBounds(model);
-                renderer.setZoomToFit(
-                        drawArea.getWidth(),
-                        drawArea.getHeight(),
-                        diagramBounds.getWidth(),
-                        diagramBounds.getHeight());
-                renderer.paintChemModel( model, visitor, drawArea, true);
-                isNew = false;
+            if (isScrolling) {
+                renderer.repaint(visitor);
             } else {
-                if (isScrolling) {
-                    renderer.repaint(visitor);
-                } else {
-                    renderer.paintChemModel(model, visitor);
-                }
+                renderer.paintChemModel(model, visitor);
             }
         }
     }
@@ -425,7 +412,8 @@ public class JChemPaintEditorWidget extends JChemPaintWidget
     public void setModel( IChemModel model ) {
         hub.setChemModel(model);
         this.applyGlobalProperties();
-
+        if(model != null)
+            getRenderer().setScale( model );
         super.setModel( model );
         setDirty( false );
     }

commit 32e83b6bbc5efcbc2c210aa682c9eb921a9666d5

Fixes a bug (#929) introduced by r9847, where molecules in JCP would not appear on screen by resetting the renderer when the chem model is set.
git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9852 fcb5ba71-d80d-0410-8237-ba3920747fcc

plugins/net.bioclipse.cdk.jchempaint/src/net/bioclipse/cdk/jchempaint/widgets/JChemPaintEditorWidget.java
@@ -412,6 +412,7 @@ public class JChemPaintEditorWidget extends JChemPaintWidget
     public void setModel( IChemModel model ) {
         hub.setChemModel(model);
         this.applyGlobalProperties();
+        getRenderer().reset();
         if(model != null)
             getRenderer().setScale( model );
         super.setModel( model );