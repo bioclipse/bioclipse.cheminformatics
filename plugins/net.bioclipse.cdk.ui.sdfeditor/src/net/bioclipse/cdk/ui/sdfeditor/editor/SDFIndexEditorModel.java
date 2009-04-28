3add36a70133ddaebcd276232f26ad84e512aaaa
goglepox <goglepox@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-04-28 08:09:56 +0000

    Recfacored out index code to a IMoleculeEditorModel
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9949 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.cdk.ui.sdfeditor/src/net/bioclipse/cdk/ui/sdfeditor/editor/SDFIndexEditorModel.java
@@ -0,0 +1,179 @@
+/*******************************************************************************
+ * Copyright (c) 2008 The Bioclipse Project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * <http://www.eclipse.org/legal/epl-v10.html>.
+ * 
+ * Contributors:
+ *     Arvid Berg <goglepox@users.sourceforge.net>
+ *     
+ ******************************************************************************/
+package net.bioclipse.cdk.ui.sdfeditor.editor;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import net.bioclipse.cdk.business.SDFileIndex;
+import net.bioclipse.cdk.domain.CDKMolecule;
+import net.bioclipse.cdk.domain.ICDKMolecule;
+import net.bioclipse.cdk.ui.views.IMoleculesEditorModel;
+import net.bioclipse.core.util.LogUtils;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.runtime.CoreException;
+import org.openscience.cdk.DefaultChemObjectBuilder;
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IChemFile;
+import org.openscience.cdk.interfaces.IChemModel;
+import org.openscience.cdk.interfaces.IChemObject;
+import org.openscience.cdk.interfaces.IChemObjectBuilder;
+import org.openscience.cdk.interfaces.IChemSequence;
+import org.openscience.cdk.interfaces.IMolecule;
+import org.openscience.cdk.io.ISimpleChemObjectReader;
+import org.openscience.cdk.io.MDLV2000Reader;
+
+
+/**
+ * @author arvid
+ *
+ */
+public class SDFIndexEditorModel implements IMoleculesEditorModel {
+
+    Logger logger = Logger.getLogger( SDFIndexEditorModel.class );
+    
+    SDFileIndex input = SDFileIndex.emptyIndex();
+    
+    protected ISimpleChemObjectReader chemReader;
+    protected IChemObjectBuilder builder;
+
+    int lastIndex = -1;
+    ICDKMolecule lastRead = null;
+    
+//    List<Object> visibleProperties= new ArrayList<Object>(10);
+    Set<Object> availableProperties= new HashSet<Object>();
+    
+    Map<Integer, ICDKMolecule> edited = new HashMap<Integer, ICDKMolecule>();
+    
+    
+    public SDFIndexEditorModel() {
+
+        chemReader = new MDLV2000Reader();
+        builder = DefaultChemObjectBuilder.getInstance();
+    }
+    
+    /* (non-Javadoc)
+     * @see net.bioclipse.cdk.ui.views.IMoleculesEditorModel#getMoleculeAt(int)
+     */
+    public ICDKMolecule getMoleculeAt( int index ) {
+
+        ICDKMolecule mol = edited.get( index );
+        if(mol == null) {
+            if(index == lastIndex) {
+                mol = lastRead;
+            }
+            else {
+                try {
+                    StringReader reader = new StringReader(getRecord( index ));
+                    chemReader.setReader( reader );
+                    IChemObject chemObj = processContent();
+                    mol = new CDKMolecule((IAtomContainer)chemObj);
+                    setLastRead( index, mol );
+                    readProperties( mol );
+                } catch ( CoreException e ) {
+                    logger.warn( "Failed to read record "+index);
+                    LogUtils.debugTrace( logger, e );
+                    setLastRead( index, null );
+                    return null;
+                } catch ( IOException e ) {
+                    logger.warn( "Failed to read record "+index);
+                    LogUtils.debugTrace( logger, e );
+                    setLastRead( index, null );
+                    return null;
+                } catch ( CDKException e ) {
+                    logger.warn( "Failed to read record "+index);
+                    LogUtils.debugTrace( logger, e );
+                    setLastRead( index, null );
+                    return null;
+                }
+            }
+        }
+        return mol;
+    }
+
+    /* (non-Javadoc)
+     * @see net.bioclipse.cdk.ui.views.IMoleculesEditorModel#getNumberOfMolecules()
+     */
+    public int getNumberOfMolecules() {
+
+        return input.size();
+    }
+
+    /* (non-Javadoc)
+     * @see net.bioclipse.cdk.ui.views.IMoleculesEditorModel#save(int, net.bioclipse.cdk.domain.ICDKMolecule)
+     */
+    public void save( int index, ICDKMolecule moleculeToSave ) {
+
+        edited.put( index, moleculeToSave );
+
+    }
+
+    private String getRecord(int index) throws CoreException, IOException {
+        InputStream in = input.file().getContents();
+        long start = input.start( index );
+        int length = (int) (input.start( index +1 )-start);
+        in.skip( start );
+        byte[] bytes= new byte[length];
+        in.read( bytes , 0  , length );
+        in.close();
+        String result = new String( bytes );
+        return result.substring( 0,result.indexOf( "$$$$" ));
+    }
+    
+    protected IChemObject processContent() throws CDKException {
+
+        IChemObject co = chemReader.read(builder.newChemFile());
+        if (co instanceof IChemFile) {
+            for(IChemSequence chemSeq:((IChemFile) co).chemSequences()) {
+                for(IChemModel chemModel:chemSeq.chemModels()) {
+                  for(IAtomContainer ac:chemModel.getMoleculeSet().molecules()) {
+                      co = (IMolecule) ac;
+                      break;
+                    }
+                  break;
+                }
+                break;
+            }
+        }
+        return co;
+    }
+    
+    private void setLastRead(int index,ICDKMolecule mol) {
+        if(mol==null) {
+            lastIndex = -1;
+        }else {
+            lastIndex = index;
+        }
+        lastRead = mol;
+    }
+    
+    private void readProperties(ICDKMolecule molecule) {
+
+        availableProperties.addAll(
+                        molecule.getAtomContainer()
+                        .getProperties().keySet());
+    }
+
+    public Collection<Object> getPropertyKeys() {
+
+        return new HashSet<Object>(availableProperties);
+    }
+
+}
