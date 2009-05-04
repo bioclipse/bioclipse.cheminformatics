bff2b81923329c599285a58f8a613ae076685743
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-04 19:41:38 +0000

    Refactored to use a List<String> to create cleaner code
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10060 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.chemoinformatics/src/net/bioclipse/chemoinformatics/util/ChemoinformaticUtils.java
+++ plugins/net.bioclipse.chemoinformatics/src/net/bioclipse/chemoinformatics/util/ChemoinformaticUtils.java
@@ -1,14 +1,22 @@
 /*****************************************************************************
- * Copyright (c) 2008 Bioclipse Project
+ * Copyright (c) 2009  Stefan Kuhn
+ *               2009  Egon Willighagen
+ * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contact: http://www.bioclipse.net/
  */
 package net.bioclipse.chemoinformatics.util;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.bioclipse.core.TestClasses;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
@@ -21,13 +29,23 @@ import org.eclipse.core.runtime.content.IContentTypeManager;
  * A class with some miscellaneous helper methods in the cheminf field. 
  * 
  * @author shk3
- *
+ * @author egonw
  */
+@SuppressWarnings("serial")
+@TestClasses("net.bioclipse.chemoinformatics.util.ChemoinformaticUtilsTest")
 public class ChemoinformaticUtils {
 
+    public final static List<String> SUPPORTED_CONTENT_TYPES =
+        new ArrayList<String>() {{
+           add("net.bioclipse.contenttypes.mdlMolFile2D");
+           add("net.bioclipse.contenttypes.mdlMolFile3D");
+           add("net.bioclipse.contenttypes.cml.singleMolecule2d");
+           add("net.bioclipse.contenttypes.cml.singleMolecule3d");
+           add("net.bioclipse.contenttypes.cml.singleMolecule5d");
+        }};
     
     /**
-     * Tells if a file is a molecule accordings to Bioclipse content types.
+     * Tells if a file is a molecule according to Bioclipse content types.
      * 
      * @param file The file to check
      * @return true=is any of the molecule content types, false= is not.
@@ -47,13 +65,8 @@ public class ChemoinformaticUtils {
         InputStream stream = file.getContents();
         IContentType contentType = contentTypeManager.findContentTypeFor(stream, file.getName());
         stream.close();
-        if(contentType!=null 
-                && (contentType.getId().equals( "net.bioclipse.contenttypes.mdlMolFile2D" )
-                || contentType.getId().equals( "net.bioclipse.contenttypes.mdlMolFile3D" )
-                || contentType.getId().equals( "net.bioclipse.contenttypes.cml.singleMolecule2d" )
-                || contentType.getId().equals( "net.bioclipse.contenttypes.cml.singleMolecule3d" )
-                || contentType.getId().equals( "net.bioclipse.contenttypes.cml.singleMolecule5d" ))
-        )
+        if (contentType != null &&
+            SUPPORTED_CONTENT_TYPES.contains(contentType.getId()))
             return true;
         else
             return false;
