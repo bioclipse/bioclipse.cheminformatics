abab6db96460a37b6e5b29590e215bcf50a6e925
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-07 13:04:05 +0000

    Added license/copyright. Solves 1025.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10115 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk.business/src/net/bioclipse/cdk/domain/CDKMoleculeSelectionHelper.java
+++ plugins/net.bioclipse.cdk.business/src/net/bioclipse/cdk/domain/CDKMoleculeSelectionHelper.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Ola Spjuth <ospjuth@users.sf.net>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contact: http://www.bioclipse.net/
+ ******************************************************************************/
 package net.bioclipse.cdk.domain;
 
 import java.util.ArrayList;
@@ -12,6 +22,11 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 
+/**
+ * A helper method to extract molecules from a selection
+ * @author ola
+ *
+ */
 public class CDKMoleculeSelectionHelper {
 
 	public static List<ICDKMolecule> getMoleculesFromSelection(ISelection selection){
