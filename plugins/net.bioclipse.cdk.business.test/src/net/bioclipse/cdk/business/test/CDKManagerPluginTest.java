81f28361ac2640a45e6d681d05fd910245a34fcf
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-10 07:42:37 +0000

    Added missing copyright header
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10195 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk.business.test/src/net/bioclipse/cdk/business/test/CDKManagerPluginTest.java
+++ plugins/net.bioclipse.cdk.business.test/src/net/bioclipse/cdk/business/test/CDKManagerPluginTest.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c)      2008  Ola Spjuth <ospjuth@users.sf.net>
+ *               2008-2009  Egon Willighagen <egonw@users.sf.net>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contact: http://www.bioclipse.net/
+ ******************************************************************************/
 package net.bioclipse.cdk.business.test;
 
 import static org.junit.Assert.fail;
