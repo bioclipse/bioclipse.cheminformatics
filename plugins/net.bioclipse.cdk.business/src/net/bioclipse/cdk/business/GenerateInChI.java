1112cab7a9fccffe7f94c201737a13bd2fa7a87f
goglepox <goglepox@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-11 14:56:59 +0000

    Aded InChI dialog
    This also adds the posibility to copy inchi with
    CMD-C
    
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10225 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk.business/src/net/bioclipse/cdk/business/GenerateInChI.java
+++ plugins/net.bioclipse.cdk.business/src/net/bioclipse/cdk/business/GenerateInChI.java
@@ -1,6 +1,8 @@
 package net.bioclipse.cdk.business;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import net.bioclipse.cdk.domain.CDKMoleculeSelectionHelper;
 import net.bioclipse.cdk.domain.ICDKMolecule;
@@ -9,10 +11,11 @@ import net.bioclipse.inchi.business.IInChIManager;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
+
+import ui.InChIDialog;
 
 public class GenerateInChI extends AbstractHandler {
 
@@ -29,7 +32,7 @@ public class GenerateInChI extends AbstractHandler {
 			System.out.println("No mols in list, exiting Inchi generation");
 
 			Shell shell=PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
-			MessageDialog.openInformation(shell, "InChI",
+			InChIDialog.openInformation(shell, "InChI",
 					"No molecules in selection.");
 			return null;
 		}
@@ -37,8 +40,10 @@ public class GenerateInChI extends AbstractHandler {
 		IInChIManager inchi=net.bioclipse.inchi.business.Activator.getDefault().
 		getInChIManager();
 
+		Map<String,String> inchiMolPairs = new HashMap<String, String>();
 		StringBuffer buffer=new StringBuffer(256);
-		buffer.append("Inchi generation for " + mols.size() + "mols:\n\n");
+		buffer.append( "Inchi generation for " + mols.size()
+		             + " molecule"+(mols.size()<=1?"":"s")+":\n\n");
 		for (ICDKMolecule cdkmol : mols){
 			String ret;
 			try {
@@ -46,13 +51,14 @@ public class GenerateInChI extends AbstractHandler {
 			} catch (Exception e) {
 				ret="ERROR: " + e.getMessage();
 			}
-			buffer.append("  * " + cdkmol.toString() + " -- " + ret + "\n");
+			inchiMolPairs.put( cdkmol.toString(), ret );
+			//buffer.append("  * " + cdkmol.toString() + " -- " + ret + "\n");
 		}
 		
 		System.out.println(buffer.toString());
 
 		Shell shell=PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
-		MessageDialog.openInformation(shell, "InChI", buffer.toString());
+		InChIDialog.openInformation(shell, "InChI", inchiMolPairs,buffer.toString());
 
 		System.out.println("INCHI generation ended");
 
