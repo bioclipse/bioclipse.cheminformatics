3ff5d81dc9bd7f5345ed4503582d45ae876928c6
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-12 21:01:08 +0000

    Added ISubStructure interface to define that an object can provide substructures for e.g. highlighting in JCP.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10284 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.cdk.business/src/net/bioclipse/cdk/domain/ISubStructure.java
@@ -0,0 +1,14 @@
+package net.bioclipse.cdk.domain;
+
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+/**
+ * An interface to provide a substructure
+ * @author ola
+ *
+ */
+public interface ISubStructure {
+
+    public IAtomContainer getAtomContainer();
+    
+}
