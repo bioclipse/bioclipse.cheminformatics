205d81192c85a0a86cef59571c554ad51e687721
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-13 07:04:31 +0000

    Added copyright and license.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10286 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk.business/src/net/bioclipse/cdk/domain/ISubStructure.java
+++ plugins/net.bioclipse.cdk.business/src/net/bioclipse/cdk/domain/ISubStructure.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+* Copyright (c) 2009  Ola Spjuth <ospjuth@users.sourceforge.net>
+*
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* www.eclipse.org—epl-v10.html <http://www.eclipse.org/legal/epl-v10.html>
+*
+* Contact: http://www.bioclipse.net/
+******************************************************************************/
 package net.bioclipse.cdk.domain;
 
 import org.openscience.cdk.interfaces.IAtomContainer;
