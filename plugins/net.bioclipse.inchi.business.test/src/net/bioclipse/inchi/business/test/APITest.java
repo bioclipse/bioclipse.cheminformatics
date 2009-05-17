e1ba08043b1ea054ebef4a5b1eaf732017323fcc
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-17 14:24:57 +0000

    Updated testing framework for the InChI manager:
    * added a suite for non-plugin tests
    * updated test class annotation in IInChIManager
    
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10488 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.inchi.business.test/src/net/bioclipse/inchi/business/test/APITest.java
+++ plugins/net.bioclipse.inchi.business.test/src/net/bioclipse/inchi/business/test/APITest.java
@@ -1,32 +1,27 @@
 /*******************************************************************************
- * Copyright (c) 2008  Egon Willighagen <egonw@users.sf.net>
- * 
+ * Copyright (c) 2008-2009  Egon Willighagen <egonw@users.sf.net>
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contact: Bioclipse Project <http://www.bioclipse.net>
+ * Contact: http://www.bioclipse.net/
  ******************************************************************************/
 package net.bioclipse.inchi.business.test;
 
-import net.bioclipse.core.tests.coverage.AbstractCoverageTest;
+import net.bioclipse.core.tests.AbstractManagerTest;
 import net.bioclipse.inchi.business.IInChIManager;
 import net.bioclipse.inchi.business.InChIManager;
 import net.bioclipse.managers.business.IBioclipseManager;
 
-/**
- * JUnit tests for checking if the tested Manager is properly tested.
- * 
- * @author egonw
- */
-public class CoverageTest extends AbstractCoverageTest {
-    
-    private static InChIManager manager = new InChIManager();
+public class APITest extends AbstractManagerTest {
+
+    InChIManager inchi = new InChIManager();
 
     @Override
     public IBioclipseManager getManager() {
-        return manager;
+        return inchi;
     }
 
     @Override
