d90f3c848b0de0945fc25229ceb6c9cd42afb3d9
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-27 20:59:47 +0000

    Implemented a NewMolecule wizard, which should open a CML file without a IFile backend
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10787 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk.jchempaint/src/net/bioclipse/cdk/jchempaint/wizards/NewMoleculeWizard.java
+++ plugins/net.bioclipse.cdk.jchempaint/src/net/bioclipse/cdk/jchempaint/wizards/NewMoleculeWizard.java
@@ -10,10 +10,23 @@
  ******************************************************************************/
 package net.bioclipse.cdk.jchempaint.wizards;
 
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.editors.text.TextEditor;
+import org.eclipse.ui.internal.editors.text.EditorsPlugin;
+import org.eclipse.ui.internal.editors.text.NonExistingFileEditorInput;
+import org.eclipse.ui.texteditor.IDocumentProvider;
 
 /**
  * Creates a new molecule and opens it in JChemPaint.
@@ -45,12 +58,51 @@ public class NewMoleculeWizard extends Wizard implements INewWizard {
     public void addPages() {
     }
 
+    public boolean canFinish() {
+        return true;
+    }
+
     public void init(IWorkbench workbench, IStructuredSelection currentSelection) {
         setWindowTitle("New Molecule");
         setNeedsProgressMonitor(true);
     }
 
     public boolean performFinish() {
+      //Open editor with content (String) as content
+
+        IFileStore fileStore= queryFileStore();
+        IEditorInput input= createEditorInput(fileStore);
+        String editorId= getEditorId(fileStore);
+        IWorkbenchPage page= PlatformUI.getWorkbench()
+            .getActiveWorkbenchWindow().getActivePage();
+        try {
+            IEditorPart editor=page.openEditor(input, editorId);
+            if (editor instanceof TextEditor) {
+                TextEditor ted = (TextEditor) editor;
+                IDocumentProvider pr=ted.getDocumentProvider();
+                IDocument doc=pr.getDocument(input);
+                //                String currentContent=doc.get();
+                doc.set(FILE_CONTENT);
+            }
+
+        } catch (PartInitException e) {
+//            LogUtils.debugTrace(logger, e);
+            e.printStackTrace();
+        }
         return true;
+    }
+
+    private IFileStore queryFileStore() {
+        IPath stateLocation= EditorsPlugin.getDefault().getStateLocation();
+        IPath path= stateLocation.append("/_" + new Object().hashCode()); //$NON-NLS-1$
+        return EFS.getLocalFileSystem().getStore(path);
+    }
+
+    private String getEditorId(IFileStore fileStore) {
+        return "net.bioclipse.cdk.ui.editors.jchempaint";
+    }
+
+    private IEditorInput createEditorInput(IFileStore fileStore) {
+        return new NonExistingFileEditorInput(fileStore, "New script");
     }
 }
