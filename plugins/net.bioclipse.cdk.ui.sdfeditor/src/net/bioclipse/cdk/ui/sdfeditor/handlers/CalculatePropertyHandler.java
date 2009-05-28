5123cd22105e45a0ce043424079862f8b1de4921
goglepox <goglepox@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-28 11:21:49 +0000

    Added command and menu item to generate property for number of atoms in molecule.
    Fixes bug #813
    The new property column dose not show up automatically, the user has to go in to the headers tab and add it to the visible coulumns
    
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10809 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.cdk.ui.sdfeditor/src/net/bioclipse/cdk/ui/sdfeditor/handlers/CalculatePropertyHandler.java
@@ -0,0 +1,74 @@
+/*******************************************************************************
+ * Copyright (c) 2009  Arvid Berg <goglepox@users.sourceforge.net>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * <http://www.eclipse.org/legal/epl-v10.html>
+ *
+ * Contact: http://www.bioclipse.net/
+ ******************************************************************************/
+package net.bioclipse.cdk.ui.sdfeditor.handlers;
+
+import net.bioclipse.cdk.ui.sdfeditor.Activator;
+import net.bioclipse.cdk.ui.sdfeditor.business.IPropertyCalculator;
+import net.bioclipse.cdk.ui.sdfeditor.editor.MoleculesEditor;
+import net.bioclipse.cdk.ui.sdfeditor.editor.MultiPageMoleculesEditorPart;
+import net.bioclipse.cdk.ui.sdfeditor.editor.SDFIndexEditorModel;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+
+/**
+ * @author arvid
+ *
+ */
+public class CalculatePropertyHandler extends AbstractHandler implements IHandler {
+
+    Logger logger = Logger.getLogger( CalculatePropertyHandler.class );
+
+    private static final String PARAMETER_ID = "net.bioclipse.cdk.ui.sdfeditor.calculatorId";
+
+    public Object execute( ExecutionEvent event ) throws ExecutionException {
+        IEditorPart editorPart = HandlerUtil.getActiveEditor( event );
+        // FIXME there can be other models besides SDFINdexEditorModel
+        MoleculesEditor editor = (MoleculesEditor) ((MultiPageMoleculesEditorPart)
+                            editorPart).getAdapter( MoleculesEditor.class );
+        if(editor== null) {
+            logger.warn( "Could not find a MoleculesEditor" );
+            return null;
+        }
+        SDFIndexEditorModel model = (SDFIndexEditorModel) editor.getModel();
+        String calc = event.getParameter( PARAMETER_ID );
+        IExtensionRegistry reg = Platform.getExtensionRegistry();
+        IConfigurationElement[] extensions;
+        IExtension ext = reg.getExtension( calc );
+        extensions = ext.getConfigurationElements();
+        for(IConfigurationElement element:extensions) {
+            element.getNamespaceIdentifier();
+            element.getName();
+            element.getContributor();
+            element.getValue();
+            try {
+                IPropertyCalculator<?> calculator = (IPropertyCalculator<?>) element.createExecutableExtension( "class" );
+                Activator.getDefault().getMoleculeTableManager().calculateProperty( model, calculator );
+                break;
+            } catch ( CoreException e ) {
+                logger.debug( "Failed to craete IPropertyCalculator", e );
+            }
+        }
+
+        return null;
+    }
+}
