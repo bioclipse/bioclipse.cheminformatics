362a52a4c6f3d5603b0661d876a0c9c55d7ecbac
goglepox <goglepox@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-06-17 11:19:15 +0000

    Added RemoveColumnHandler
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@11386 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.cdk.ui.sdfeditor/src/net/bioclipse/cdk/ui/sdfeditor/handlers/RemoveColumnHandler.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2009  Arvid Berg <goglepox@users.sourceforge.net>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * <http://www.eclipse.org/legal/epl-v10.html>
+ *
+ * Contact: http://www.bioclipse.net/
+ ******************************************************************************/
+package net.bioclipse.cdk.ui.sdfeditor.handlers;
+
+import net.bioclipse.cdk.ui.sdfeditor.editor.MoleculeTableContentProvider;
+import net.bioclipse.cdk.ui.sdfeditor.editor.MoleculesEditor;
+import net.sourceforge.nattable.NatTable;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+
+public class RemoveColumnHandler extends AbstractHandler implements IHandler {
+
+    public Object execute( ExecutionEvent event ) throws ExecutionException {
+
+        IEditorPart part = HandlerUtil.getActiveEditor( event );
+        MoleculesEditor molEditor = (MoleculesEditor)
+                                    part.getAdapter( MoleculesEditor.class );
+
+        int[] vals  = ((NatTable)molEditor.getMolTableViewer().getControl())
+                                .getSelectionModel().getSelectedColumns();
+        MoleculeTableContentProvider contentProvider =
+                                                molEditor.getContentProvider();
+
+        for(int i:vals) {
+            Object o = contentProvider.getProperties().get( i-1 );
+            contentProvider.removeColumn( o );
+        }
+        return null;
+    }
+
+}
