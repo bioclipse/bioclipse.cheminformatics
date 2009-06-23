ed3270c6569834497bb23c459aa5d1c405b5c592
goglepox <goglepox@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-06-23 09:22:18 +0000

    Added copyright/license statement
    see bug #1333
    
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@11474 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk.jchempaint.view/src/net/bioclipse/cdk/jchempaint/view/SWTFontManager.java
+++ plugins/net.bioclipse.cdk.jchempaint.view/src/net/bioclipse/cdk/jchempaint/view/SWTFontManager.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2009  Arvid Berg <goglepox@users.sourceforge.net>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * <http://www.eclipse.org/legal/epl-v10.html>
+ *
+ * Contact: http://www.bioclipse.net/
+ ******************************************************************************/
 package net.bioclipse.cdk.jchempaint.view;
 
 import java.util.ArrayList;
--- plugins/net.bioclipse.cdk.jchempaint/src/net/bioclipse/cdk/jchempaint/undoredo/SWTMergeMoleculesEdit.java
+++ plugins/net.bioclipse.cdk.jchempaint/src/net/bioclipse/cdk/jchempaint/undoredo/SWTMergeMoleculesEdit.java
@@ -18,6 +18,16 @@ import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IBond;
 
+/*******************************************************************************
+ * Copyright (c) 2009  Arvid Berg <goglepox@users.sourceforge.net>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * <http://www.eclipse.org/legal/epl-v10.html>
+ *
+ * Contact: http://www.bioclipse.net/
+ ******************************************************************************/
 public class SWTMergeMoleculesEdit extends MergeMoleculesEdit implements
         IUndoableOperation {
 
