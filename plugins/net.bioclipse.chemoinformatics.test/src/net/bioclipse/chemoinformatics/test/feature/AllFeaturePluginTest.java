fc34f6e85be076e977efaf2888949714c93d74fb
Egon Willighagen <egon.willighagen@gmail.com>
2009-10-20 15:52:08 +0200

    Added tests for all the plugins in this feature (at least, in the future; now only the CDK tests included)

---
+++ plugins/net.bioclipse.chemoinformatics.test/src/net/bioclipse/chemoinformatics/test/feature/AllFeaturePluginTest.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2009  Egon Willighagen <egonw@users.sf.net>
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contact: Bioclipse Project <http://www.bioclipse.net>
+ ******************************************************************************/
+package net.bioclipse.chemoinformatics.test.feature;
+
+import net.bioclipse.cdk.AllCDKBusinessPluginTestSuite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * JUnit testing for all managers in this feature.
+ * 
+ * @author egonw
+ */
+@RunWith(value=Suite.class)
+@SuiteClasses(value={
+    AllCDKBusinessPluginTestSuite.class
+})
+public class AllFeaturePluginTest {}
---
+++ plugins/net.bioclipse.chemoinformatics.test/src/net/bioclipse/chemoinformatics/test/feature/AllFeatureTest.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2009  Egon Willighagen <egonw@users.sf.net>
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contact: Bioclipse Project <http://www.bioclipse.net>
+ ******************************************************************************/
+package net.bioclipse.chemoinformatics.test.feature;
+
+import net.bioclipse.cdk.AllCDKBusinessTestsSuite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * JUnit testing for all plugins in this feature.
+ * 
+ * @author egonw
+ */
+@RunWith(value=Suite.class)
+@SuiteClasses(value={
+    FeatureTest.class,
+    AllCDKBusinessTestsSuite.class
+})
+public class AllFeatureTest {
+
+}
