d2914f8b24740faed18df3d6fee564e66a24b153
Arvid Berg <goglepox@users.sourceforge.net>
2011-01-20 14:36:07 +0100

    Added launch configuration and test suit for running all chemoinformatics plugin-tests

---
+++ plugins/net.bioclipse.cdk.business.test/src/net/bioclipse/cdk/ChemoinformaticsPluginTestSuite.java
@@ -0,0 +1,25 @@
+/* *****************************************************************************
+* Copyright (c) 2011 Arvid Berg <goglepox@users.sourceforge.net
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*   Arvid Berg
+******************************************************************************/
+package net.bioclipse.cdk;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author arvid
+ *
+ */
+@RunWith(value=Suite.class)
+@SuiteClasses( value = { AllCDKBusinessPluginTestSuite.class } )
+public class ChemoinformaticsPluginTestSuite {
+
+}
