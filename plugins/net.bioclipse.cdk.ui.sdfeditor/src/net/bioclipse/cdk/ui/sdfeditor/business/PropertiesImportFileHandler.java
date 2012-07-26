b826f2145de2d54d99af9be6f4ba940d16c2a173
Klas Jönsson <klas.joensson@gmail.com>
2012-07-26 14:26:19 +0200

    Fixed the copy-right text

--- plugins/net.bioclipse.cdk.ui.sdfeditor/src/net/bioclipse/cdk/ui/sdfeditor/business/PropertiesImportFileHandler.java
+++ plugins/net.bioclipse.cdk.ui.sdfeditor/src/net/bioclipse/cdk/ui/sdfeditor/business/PropertiesImportFileHandler.java
@@ -1,9 +1,10 @@
 /* *****************************************************************************
- * Copyright (c) 2006, 2008-2009 IBM Corporation and others.
+ * Copyright (c) 2007-2012 Bioclipse Project
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ *    
  ******************************************************************************/
 package net.bioclipse.cdk.ui.sdfeditor.business;
 
