75356d597193b53f141081f40b9c616d38a4ffb8
Klas Jönsson <klas.joensson@gmail.com>
2012-11-05 10:23:46 +0100

    Adds a link to the chemspider homepage on the chemspider preferences page

--- plugins/net.bioclipse.chemspider/src/net/bioclipse/chemspider/ui/ChemspiderPreferencePage.java
+++ plugins/net.bioclipse.chemspider/src/net/bioclipse/chemspider/ui/ChemspiderPreferencePage.java
@@ -10,33 +10,63 @@
  ******************************************************************************/
 package net.bioclipse.chemspider.ui;
 
- import net.bioclipse.chemspider.Activator;
+ import java.net.MalformedURLException;
+import java.net.URL;
 
+import net.bioclipse.chemspider.Activator;
+import org.apache.log4j.Logger;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Link;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 
 /**
  * 
- * @author Ola Spjuth
+ * @author Ola Spjuth, Klas Jšnsson
  */
 public class ChemspiderPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage{
 
+    private static final Logger logger = Logger.getLogger(ChemspiderPreferencePage.class);
+    
     public void init(IWorkbench workbench) {
         //Initialize the preference store we wish to use
         setPreferenceStore(Activator.getDefault().getPreferenceStore());
 
       }
     
-
     @Override
     protected void createFieldEditors() {
-
+        Composite parent = getFieldEditorParent();
     	StringFieldEditor token = new StringFieldEditor(Activator.PREF_SECURITY_TOKEN, 
-    					"Chemspider Security Token" +
-    					"(get one at \nhttp://www.chemspider.com/AboutServices.aspx)", 
-    				    getFieldEditorParent());
+    					"Chemspider Security Token (get one at:", parent);
+    	Link link = new Link(parent, SWT.UNDERLINE_LINK);
+    	link.setText( "<a>http://www.chemspider.com/AboutServices.aspx</a>)" );
+    	link.addMouseListener( new MouseAdapter() {
+    	    @Override
+    	    public void mouseDown(MouseEvent me) {
+    	        try {
+                    //  Open default external browser 
+                    PlatformUI.getWorkbench().getBrowserSupport().
+                    getExternalBrowser().
+                    openURL(new URL("http://www.chemspider.com/AboutServices.aspx"));
+                } 
+                catch (PartInitException ex) {
+                    logger.error( ex );
+                    ex.printStackTrace();
+                } 
+                catch (MalformedURLException ex) {
+                    logger.error( ex );
+                    ex.printStackTrace();
+                }
+    	    }
+        } );
 
     	StringFieldEditor endpoint = new StringFieldEditor(Activator.PREF_SERVER_ENDPOINT, 
 				"Chemspider server endpoint", 
