3c418edb6d21d9ae90307f621822992730dfe393
Klas Jönsson <klas.joensson@gmail.com>
2013-01-18 16:45:18 +0100

    Adding adapters for CML-/SMILES-molecules to CDK-molecules.
    The CMLAdapterFactory is an adapter for CML-molecules to CDK-molecules, and
    the SMILESAdapterFactory is an adapter for SMILES-molecules to CDK-molecules.

---
+++ plugins/net.bioclipse.cdk.business/src/net/bioclipse/cdk/domain/SMILESAdapterFactory.java
@@ -0,0 +1,56 @@
+/* *****************************************************************************
+ *Copyright (c) 2013 The Bioclipse Team and others.
+ *All rights reserved. This program and the accompanying materials
+ *are made available under the terms of the Eclipse Public License v1.0
+ *which accompanies this distribution, and is available at
+ *http://www.eclipse.org/legal/epl-v10.html
+ *
+ ******************************************************************************/
+package net.bioclipse.cdk.domain;
+
+import net.bioclipse.cdk.business.Activator;
+import net.bioclipse.core.business.BioclipseException;
+import net.bioclipse.core.domain.SMILESMolecule;
+import net.bioclipse.core.util.LogUtils;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.runtime.IAdapterFactory;
+
+/**
+ *
+ * @author Klas Jšnsson (klas.joensson@gmail.com)
+ *
+ */
+public class SMILESAdapterFactory implements IAdapterFactory {
+
+    Logger logger = Logger.getLogger( SMILESAdapterFactory.class );
+    
+    @Override
+    public Object getAdapter( Object adaptableObject, Class adapterType ) {
+        
+        Object molecule = null;
+        if ( adaptableObject instanceof SMILESMolecule ) {
+            SMILESMolecule mol = (SMILESMolecule) adaptableObject;
+            if (adapterType.equals( SMILESMolecule.class )) {
+                try {
+                    molecule = Activator.getDefault()
+                            .getJavaCDKManager().fromSMILES( mol.toSMILES() );
+                } catch ( BioclipseException e ) {
+                    logger.debug( LogUtils.traceStringOf( e ));
+                    return null;
+                }
+            }
+            
+        }
+        if(molecule !=null &&adapterType.isAssignableFrom( molecule.getClass()))
+            return molecule;
+        
+        return null;
+    }
+
+    @Override
+    public Class[] getAdapterList() {
+        return new Class[] { SMILESMolecule.class };
+    }
+
+}
